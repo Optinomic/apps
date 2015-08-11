28a74366ad425f841d42fe6ea69a4bcd8b27f7cb
Beat Ottiger <ottigerb@gmail.com>
2015-08-11 14:10:29 +0200

    wip

--- craving/main.js
+++ craving/main.js
@@ -1,3 +1,24 @@
-(function() {
+'use strict';
 
-})();
+/**
+ * @name optinomic - App
+ *
+ * Main module of the application.
+ */
+angular.module('optinomicApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngRoute',
+    'ngMaterial'
+]);
+
+
+angular.controller('AppCtrl', function($scope) {
+    $scope.awesomeThings = [
+        'HTML5 Boilerplate',
+        'AngularJS',
+        'Karma'
+    ];
+
+    console.log('Hello ANGULAR!', $scope.awesomeThings);
+});
