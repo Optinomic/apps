b068c2aedd99f1254135c6a973f9c17470be4705
Beat Ottiger <ottigerb@gmail.com>
2015-08-12 16:36:51 +0200

    wip

--- craving/main.js
+++ craving/main.js
@@ -6,7 +6,7 @@
 app.controller('AppCtrl', function($scope, $routeParams, $location) {
     this.app = {
         'title': 'Craving-App',
-        'subtitle': 'Some Text Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        'subtitle': 'Some Text Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod labore et dolore ullamco laboris nisi ut aliquip ex ea commodo consequat.'
     };
 
     console.log('Welcome, ', this.app.title);
