dd8b6b8ccda72f8326202458d995be49c20e2121
Beat Ottiger <ottigerb@gmail.com>
2015-08-12 10:39:42 +0200

    wip

--- craving/main.js
+++ craving/main.js
@@ -3,7 +3,7 @@
  * --------------------------------------
  * Main ctl of the Optinomic-Application.
  */
-app.controller('AppCtrl', function() {
+app.controller('AppCtrl', function($scope) {
     this.app = {
         'title': 'Craving-App',
         'subtitle': 'Some Text Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
