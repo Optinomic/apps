e2248322511395c98fd8d2a6eabf1a1856355403
Beat Ottiger <ottigerb@gmail.com>
2015-08-12 10:54:54 +0200

    wip

--- craving/main.js
+++ craving/main.js
@@ -9,6 +9,8 @@ app.controller('AppCtrl', function($scope) {
         'subtitle': 'Some Text Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
     };
 
-    console.log('Welcome, ', this.app.title);
+    this.welcome = 'Welcome';
+
+    console.log('Welcome, ', this.app);
 
 });
