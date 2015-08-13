b07596b151a930891b9935cc4205341b40221a93
Beat Ottiger <ottigerb@gmail.com>
2015-08-13 17:19:27 +0200

    wip

--- craving/main.js
+++ craving/main.js
@@ -3,18 +3,42 @@
  * ---------------------------------------
  * Controller of the Optinomic-Application.
  */
-app.controller('AppCtrl', function($scope, $routeParams, $location, resultsService) {
+app.controller('AppCtrl', function($scope, dataService) {
+
+    // -----------------------------------
+    // Init
+    // -----------------------------------
+    $scope.init = false;
+
     this.app = {
+        'id': 'com.optinomic.apps.craving',
         'title': 'Craving-App',
         'subtitle': 'Some Text Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod labore et dolore ullamco laboris nisi ut aliquip ex ea commodo consequat.'
     };
 
     console.log('Welcome, ', this.app.title);
+    console.log('Token / PID ', helpers.getToken(), helpers.getPatientID());
 
 
-    console.log('$routeParams, ', $routeParams);
-    console.log('$location, ', $location, $location.search());
 
-    console.log('Token = ', helpers.getToken());
+    // -----------------------------------
+    // Functions
+    // -----------------------------------
+
+    $scope.loadMainData = function() {
+        // -----------------------------------
+        // Get Data 
+        // -----------------------------------
+        var dataPromiseMain = dataService.getMainAppData(this.app.id);
+        dataPromiseMain.then(function(data) {
+
+            console.log('(DATA): getMainAppData:', data);
+
+
+            $scope.init = true;
+        });
+    };
+    $scope.loadMainData();
+
 
 });
