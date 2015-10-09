5bd833077831aa25a3632fb6f2092f4e91785f58
Beat Ottiger <ottigerb@gmail.com>
2015-10-09 19:40:28 +0200

    wip

--- bscl_anq/calculations/scores.js
+++ bscl_anq/calculations/scores.js
@@ -15,22 +15,19 @@ function main(responses) {
             var result = response.data.response;
 
             myResults.sum_scores = {};
-            myResults.sum_scores.aggr = 1 + 2 + 3;
+            myResults.sum_scores.aggr = parseInt(result.BSCL[sq504V06]) + parseInt(result.BSCL[BSCL_sq504V13]) + parseInt(result.BSCL[BSCL_sq504V40]) + parseInt(result.BSCL[BSCL_sq504V46]) + parseInt(result.BSCL[sq504V06]);
 
-            // var myResults.sum_scores = {};
-            // myResults.sum_scores.aggr = 1 + 2 + 3;
-            //myResults.sum_scores.aggr = parseInt(BSCL[sq504V06]) + parseInt(BSCL[BSCL_sq504V13]) + parseInt(BSCL[BSCL_sq504V40]) + parseInt(BSCL[BSCL_sq504V46]) + parseInt(BSCL[sq504V06]);
-            //    result.sum_scores.angst = parseInt(BSCL[BSCL_sq504V01]) + parseInt(BSCL[BSCL_sq504V12]) + parseInt(BSCL[BSCL_sq504V19]) + parseInt(BSCL[BSCL_sq504V38]) + parseInt(BSCL[BSCL_sq504V45]) + parseInt(BSCL[BSCL_sq504V49]);
-            //    result.sum_scores.depr = parseInt(BSCL[BSCL_sq504V09]) + parseInt(BSCL[BSCL_sq504V16]) + parseInt(BSCL[BSCL_sq504V17]) + parseInt(BSCL[BSCL_sq504V18]) + parseInt(BSCL[BSCL_sq504V35]) + parseInt(BSCL[BSCL_sq504V50]);
-            //    result.sum_scores.paranoid = parseInt(BSCL[BSCL_sq504V04]) + parseInt(BSCL[BSCL_sq504V10]) + parseInt(BSCL[BSCL_sq504V24]) + parseInt(BSCL[BSCL_sq504V48]) + parseInt(BSCL[BSCL_sq504V51]);
-            //    result.sum_scores.phobisch = parseInt(BSCL[BSCL_sq504V08]) + parseInt(BSCL[BSCL_sq504V28]) + parseInt(BSCL[BSCL_sq504V31]) + parseInt(BSCL[BSCL_sq504V43]) + parseInt(BSCL[BSCL_sq504V47]);
-            //    result.sum_scores.psychot = parseInt(BSCL[BSCL_sq504V03]) + parseInt(BSCL[BSCL_sq504V14]) + parseInt(BSCL[BSCL_sq504V34]) + parseInt(BSCL[BSCL_sq504V44]) + parseInt(BSCL[BSCL_sq504V53]);
-            //    result.sum_scores.somat = parseInt(BSCL[BSCL_sq504V02]) + parseInt(BSCL[BSCL_sq504V23]) + parseInt(BSCL[BSCL_sq504V29]) + parseInt(BSCL[BSCL_sq504V30]) + parseInt(BSCL[BSCL_sq504V33]) + parseInt(BSCL[BSCL_sq504V37]);
-            //    result.sum_scores.soz = parseInt(BSCL[BSCL_sq504V20]) + parseInt(BSCL[BSCL_sq504V21]) + parseInt(BSCL[BSCL_sq504V22]) + parseInt(BSCL[BSCL_sq504V42]);
-            //    result.sum_scores.zwang = parseInt(BSCL[BSCL_sq504V05]) + parseInt(BSCL[BSCL_sq504V15]) + parseInt(BSCL[BSCL_sq504V26]) + parseInt(BSCL[BSCL_sq504V27]) + parseInt(BSCL[BSCL_sq504V32]) + parseInt(BSCL[BSCL_sq504V36]);
-            //    result.sum_scores.zusatz = parseInt(BSCL[BSCL_sq504V11]) + parseInt(BSCL[BSCL_sq504V25]) + parseInt(BSCL[BSCL_sq504V39]) + parseInt(BSCL[BSCL_sq504V52]);
+            //    result.sum_scores.angst = parseInt(result.BSCL[BSCL_sq504V01]) + parseInt(result.BSCL[BSCL_sq504V12]) + parseInt(result.BSCL[BSCL_sq504V19]) + parseInt(result.BSCL[BSCL_sq504V38]) + parseInt(result.BSCL[BSCL_sq504V45]) + parseInt(result.BSCL[BSCL_sq504V49]);
+            //    result.sum_scores.depr = parseInt(result.BSCL[BSCL_sq504V09]) + parseInt(result.BSCL[BSCL_sq504V16]) + parseInt(result.BSCL[BSCL_sq504V17]) + parseInt(result.BSCL[BSCL_sq504V18]) + parseInt(result.BSCL[BSCL_sq504V35]) + parseInt(result.BSCL[BSCL_sq504V50]);
+            //    result.sum_scores.paranoid = parseInt(result.BSCL[BSCL_sq504V04]) + parseInt(result.BSCL[BSCL_sq504V10]) + parseInt(result.BSCL[BSCL_sq504V24]) + parseInt(result.BSCL[BSCL_sq504V48]) + parseInt(result.BSCL[BSCL_sq504V51]);
+            //    result.sum_scores.phobisch = parseInt(result.BSCL[BSCL_sq504V08]) + parseInt(result.BSCL[BSCL_sq504V28]) + parseInt(result.BSCL[BSCL_sq504V31]) + parseInt(result.BSCL[BSCL_sq504V43]) + parseInt(result.BSCL[BSCL_sq504V47]);
+            //    result.sum_scores.psychot = parseInt(result.BSCL[BSCL_sq504V03]) + parseInt(result.BSCL[BSCL_sq504V14]) + parseInt(result.BSCL[BSCL_sq504V34]) + parseInt(result.BSCL[BSCL_sq504V44]) + parseInt(result.BSCL[BSCL_sq504V53]);
+            //    result.sum_scores.somat = parseInt(result.BSCL[BSCL_sq504V02]) + parseInt(result.BSCL[BSCL_sq504V23]) + parseInt(result.BSCL[BSCL_sq504V29]) + parseInt(result.BSCL[BSCL_sq504V30]) + parseInt(result.BSCL[BSCL_sq504V33]) + parseInt(result.BSCL[BSCL_sq504V37]);
+            //    result.sum_scores.soz = parseInt(result.BSCL[BSCL_sq504V20]) + parseInt(result.BSCL[BSCL_sq504V21]) + parseInt(result.BSCL[BSCL_sq504V22]) + parseInt(result.BSCL[BSCL_sq504V42]);
+            //    result.sum_scores.zwang = parseInt(result.BSCL[BSCL_sq504V05]) + parseInt(result.BSCL[BSCL_sq504V15]) + parseInt(result.BSCL[BSCL_sq504V26]) + parseInt(result.BSCL[BSCL_sq504V27]) + parseInt(result.BSCL[BSCL_sq504V32]) + parseInt(result.BSCL[BSCL_sq504V36]);
+            //    result.sum_scores.zusatz = parseInt(result.BSCL[BSCL_sq504V11]) + parseInt(result.BSCL[BSCL_sq504V25]) + parseInt(result.BSCL[BSCL_sq504V39]) + parseInt(result.BSCL[BSCL_sq504V52]);
             //
-            //    var result.scale_scores = {};
+            //    result.scale_scores = {};
             //    result.scale_scores.aggr = parseFloat(result.sum_scores.aggr / 5);
             //    result.scale_scores.angst = parseFloat(result.sum_scores.angst / 6);
             //    result.scale_scores.depr = parseFloat(result.sum_scores.depr / 6);
