5bf6e0c1469f21844a297efc81d4ee6a96d716a4
Beat Ottiger <ottigerb@gmail.com>
2015-10-10 12:26:00 +0200

    wip

--- angular_template/calculations/another_calculation.js
+++ angular_template/calculations/another_calculation.js
@@ -1,6 +1,43 @@
 function main(responses) {
-    return {
-        "a": 66.9,
-        "responses": responses
+    var calc = {};
+
+    // ------------------------------------------
+    // H e l p e r   -   F U N C T I O N S
+    // ------------------------------------------
+
+    calc.doSomething = function() {
+        var score = 73;
+        return t_score;
     };
+
+
+    // ------------------------------------------
+    // F U N C T I O N  -  Main
+    // ------------------------------------------
+    calc.getResults = function(myResponses) {
+
+        var responses_array = myResponses.survey_responses;
+        var allResults = [];
+
+        responses_array.forEach(function(response, myindex) {
+            var myResults = {};
+            var result = response.data.response;
+
+            // Something
+            myResults.something = calc.doSomething();
+
+
+            // Write Results for the Return
+            // Do not modify stuff here
+            myResults.hash = result['optinomixHASH'];
+            myResults.response = response;
+            allResults.push(myResults);
+        });
+
+        return allResults;
+    };
+
+
+    // Return
+    return calc.getResults(responses);
 }
