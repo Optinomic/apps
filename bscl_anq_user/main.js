f9e0f9e88c4fc3d2ec5d5a302798405b186e7c47
Beat Ottiger <ottigerb@gmail.com>
2015-11-02 17:18:48 +0100

    wip

--- bscl_anq_user/main.js
+++ bscl_anq_user/main.js
@@ -80,12 +80,12 @@ app.controller('AppCtrl', function($scope, dataService, scopeDService) {
             });
         });
 
-        var all_groups = $scope.d.dataMain.patient_groups;
+        var all_groups = my_return.patient_groups;
         all_groups.forEach(function(patient_group, myindex) {
 
-            var my_results = patient_group[patient_group.data.name].results === undefined ? [] : patient_group[patient_group.data.name].results;
+            //var my_results = patient_group[patient_group.data.name].results === undefined ? [] : patient_group[patient_group.data.name].results;
 
-            console.log('Resultate: ', patient_group.data.name, my_results);
+            console.log('Resultate: ', patient_group);
 
         });
 
