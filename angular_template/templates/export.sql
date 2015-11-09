2c24c9a6f4de2e0bf1721c6699b7090e232d596f
Beat Ottiger <ottigerb@gmail.com>
2015-11-09 11:11:02 +0100

    wip

--- angular_template/templates/export.sql
+++ angular_template/templates/export.sql
@@ -1,8 +1,8 @@
-SELECT \
-  patient.id \
-  , patient.last_name \
-  , ((cast(response AS json))->>'BSCL[sq504V40]') as gaga \
-  , recode_into(((cast(response AS json))->>'BSCL[sq504V40]'), '', '-1')  as sq504V40 \
-  , recode_into(((cast(response AS json))->>'BSCL[sq504V40]'), '', '0') + 2  as gaga \
-FROM survey_response INNER JOIN patient ON(survey_response.patient = patient.id) \
+SELECT 
+  patient.id 
+  , patient.last_name 
+  , ((cast(response AS json))->>'BSCL[sq504V40]') as gaga 
+  , recode_into(((cast(response AS json))->>'BSCL[sq504V40]'), '', '-1')  as sq504V40 
+  , recode_into(((cast(response AS json))->>'BSCL[sq504V40]'), '', '0') + 2  as gaga 
+FROM survey_response INNER JOIN patient ON(survey_response.patient = patient.id) 
 WHERE module = 'ch.suedhang.apps.bscl.anq'
