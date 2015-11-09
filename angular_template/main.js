ee52b0e3f80773b2804c4e78e90e6b1af7e81ca0
Beat Ottiger <ottigerb@gmail.com>
2015-11-09 10:55:32 +0100

    wip

--- angular_template/main.js
+++ angular_template/main.js
@@ -61,6 +61,12 @@ app.controller('AppCtrl', function($scope, dataService, scopeDService) {
     $scope.d.export.delimitter = ';';
     $scope.d.export.sql_field = "select * from information_schema.tables";
 
+    $scope.d.export.sql_field = "SELECT patient.id , patient.last_name , ((cast(response AS json))->>'BSCL[sq504V40]') as gaga , recode_into(((cast(response AS json))->>'BSCL[sq504V40]'), '', '-1') as sq504V40 , recode_into(((cast(response AS json))->>'BSCL[sq504V40]'), '', '0') + 2 as gaga FROM survey_response INNER JOIN patient ON(survey_response.patient = patient.id) WHERE module = 'ch.suedhang.apps.bscl.anq'";
+
+
+    $scope.d.export.sql_field = "include(`templates/export.sql')";
+    $scope.d.export.sql_field = $scope.d.export.sql_field.join(' *\n *');
+
     //$scope.d.export.sql_field = "SELECT 
     //patient.id, patient.last_name, ((cast(response AS json)) - >> 'BSCL[sq504V40]') as gaga, recode_into(((cast(response AS json)) - >> 'BSCL[sq504V40]'), '', '-1') as sq504V40, recode_into(((cast(response AS json)) - >> 'BSCL[sq504V40]'), '', '0') + 2 as gaga
     //FROM survey_response INNER JOIN patient ON(survey_response.patient = patient.id)
