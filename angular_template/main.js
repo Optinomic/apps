74f50d8318065413d63cd32954dc15fc0ee991ff
Beat Ottiger <ottigerb@gmail.com>
2015-11-09 11:12:29 +0100

    wip

--- angular_template/main.js
+++ angular_template/main.js
@@ -64,7 +64,9 @@ app.controller('AppCtrl', function($scope, dataService, scopeDService) {
     $scope.d.export.sql_field = "SELECT patient.id , patient.last_name , ((cast(response AS json))->>'BSCL[sq504V40]') as gaga , recode_into(((cast(response AS json))->>'BSCL[sq504V40]'), '', '-1') as sq504V40 , recode_into(((cast(response AS json))->>'BSCL[sq504V40]'), '', '0') + 2 as gaga FROM survey_response INNER JOIN patient ON(survey_response.patient = patient.id) WHERE module = 'ch.suedhang.apps.bscl.anq'";
 
 
-    //var sql_import_string = "include(`templates/export.sql')";
+    // ToDO: M4 - Import - remove new lines.
+
+    //var sql_import_string = "in_clude(`templates/export.sql')";
     //$scope.d.export.sql_field = sql_import_string.join(' *\n *');
 
     //$scope.d.export.sql_field = "SELECT 
