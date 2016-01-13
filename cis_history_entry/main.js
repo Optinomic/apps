59fca4f36b3982a246d33eee3dc833163befcc74
Beat Ottiger <ottigerb@gmail.com>
2016-01-13 16:38:45 +0100

    wip

--- cis_history_entry/main.js
+++ cis_history_entry/main.js
@@ -54,7 +54,7 @@ app.controller('AppCtrl', function($scope, dataService, scopeDService) {
         var api_call = dataService.getPatientAnnotationsData($scope.d.nodeTree);
         api_call.then(function(data) {
             // Create Array if not already exists.
-            $scope.d.entrys.historyEntrys = data.historyEntrys === undefined ? [] : data.historyEntrys;
+            $scope.d.historyEntrys = data.historyEntrys === undefined ? [] : data.historyEntrys;
             console.log('(+) getHisoryPosts ', data, $scope.d.entrys.historyEntrys);
         });
 
