0196bb721f0b431f927844d5450103ef7c77c5f9
Beat Ottiger <ottigerb@gmail.com>
2016-01-17 16:27:01 +0100

    select diagnoses

--- cis_diagnoses/main.js
+++ cis_diagnoses/main.js
@@ -123,15 +123,70 @@ app.controller('AppCtrl', function($scope, $http, $filter, $mdDialog, dataServic
     $scope.appInit = function() {
         $scope.d.nodeTree = 'diagnoses';
 
-        $scope.d.appInit = {
-            filter: '',
-            predicate: 'datum_sort',
-            reverse: true,
-            debug: false
-        };
+        $scope.d.appInit = {};
+        var self = $scope.d.appInit;
+        self.simulateQuery = false;
+        self.isDisabled = false;
+        self.repos = loadAll();
+        self.querySearch = querySearch;
+        self.selectedItemChange = selectedItemChange;
+        self.searchTextChange = searchTextChange;
 
         $scope.loadICD10Sheet();
     };
+
+
+
+
+
+    // ******************************
+    // Internal methods
+    // ******************************
+    /**
+     * Search for repos... use $timeout to simulate
+     * remote dataservice call.
+     */
+    function querySearch(query) {
+        var results = query ? self.repos.filter(createFilterFor(query)) : self.repos,
+            deferred;
+        if (self.simulateQuery) {
+            deferred = $q.defer();
+            $timeout(function() {
+                deferred.resolve(results);
+            }, Math.random() * 1000, false);
+            return deferred.promise;
+        } else {
+            return results;
+        }
+    }
+
+    function searchTextChange(text) {
+        $log.info('Text changed to ' + text);
+    }
+
+    function selectedItemChange(item) {
+        $log.info('Item changed to ' + JSON.stringify(item));
+    }
+    /**
+     * Build `components` list of key/value pairs
+     */
+    function loadAll() {
+        var catalog = $scope.d.ICD10_all;
+        return catalog.map(function(diagn) {
+            diagn.value = diagn.icd_code;
+            return diagn;
+        });
+    }
+    /**
+     * Create filter function for a query string
+     */
+    function createFilterFor(query) {
+        var lowercaseQuery = angular.lowercase(query);
+        return function filterFn(item) {
+            return (item.value.indexOf(lowercaseQuery) === 0);
+        };
+    }
+
 
 
     //$scope.getUserSettings = function() {
