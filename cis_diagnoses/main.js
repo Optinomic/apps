511c5e99d4945515567ed0274cfedfe00f79ecd0
Beat Ottiger <ottigerb@gmail.com>
2016-01-18 16:06:29 +0100

    wip

--- cis_diagnoses/main.js
+++ cis_diagnoses/main.js
@@ -139,6 +139,7 @@ app.controller('AppCtrl', function($scope, $http, $filter, $mdDialog, dataServic
         $scope.d.appInit.selectedItemChange = selectedItemChange;
         $scope.d.appInit.searchTextChange = searchTextChange;
         $scope.d.appInit.searchText = '';
+        $scope.d.appInit.autofocus = true
 
         $scope.d.init = true;
         $scope.d.haveData = true;
