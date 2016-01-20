1f88639bca13d4ec57987fae8889a1b73a309b03
Beat Ottiger <ottigerb@gmail.com>
2016-01-20 18:34:17 +0100

    wip

--- cis_history_entry/main.js
+++ cis_history_entry/main.js
@@ -337,9 +337,15 @@ app.controller('AppCtrl', function($scope, $http, $filter, $mdDialog, dataServic
     };
 
 
+
+    // Vaadin Chart Example
+    $scope.supplyData = [35, 28, 45, 60, 80, 74];
+    $scope.demandData = [29, 11, 50, 63, 65, 61];
+
+
 });
 
-app.directive('areaSpline', function() {
+angular.module('optinomicDataModule').directive('areaSpline', function() {
     return {
         scope: {
             supply: '=',
