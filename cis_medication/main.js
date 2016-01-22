3587c19a19702f7ad47235929761a5dc6cf80716
Beat Ottiger <ottigerb@gmail.com>
2016-01-22 15:28:25 +0100

    wip

--- cis_medication/main.js
+++ cis_medication/main.js
@@ -125,7 +125,6 @@ app.controller('AppCtrl', function($scope, $http, $filter, $mdDialog, dataServic
         $scope.d.appInit.searchTextChange = searchTextChange;
         $scope.d.appInit.searchText = '';
         $scope.d.appInit.autofocus = true;
-        $scope.d.appInit.medi_not_found = false;
 
         $scope.d.appInit.show_controls = [];
 
