8804ce35001d4a12088ec5b7da7de2ee33e49cb7
Beat Ottiger <ottigerb@gmail.com>
2016-01-27 22:46:11 +0100

    wip

--- cis_medication/main.js
+++ cis_medication/main.js
@@ -333,7 +333,7 @@ app.controller('AppCtrl', function($scope, $http, $filter, $mdDialog, dataServic
                 $scope.d.medication = angular.copy(data);
             };
 
-
+            $scope.setDataView();
             $scope.d.appState = 'show';
 
             //console.log('(+) getEntrys ', $scope.d.medication);
