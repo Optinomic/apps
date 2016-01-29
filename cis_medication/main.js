967ff320e7faaba5d614ae6e9dbe205b188c7f43
Beat Ottiger <ottigerb@gmail.com>
2016-01-29 18:56:22 +0100

    wip

--- cis_medication/main.js
+++ cis_medication/main.js
@@ -736,6 +736,18 @@ app.controller('AppCtrl', function($scope, $http, $filter, $mdDialog, dataServic
         $scope.d.grid.options.rowData = new_data;
         $scope.d.grid.options.api.setRowData(new_data);
 
+        // Sorting
+
+        var sort = [{
+            colId: medication_status,
+            sort: 'asc'
+        }, {
+            colId: medication_name,
+            sort: 'asc'
+        }];
+
+        $scope.d.grid.options.api.setSortModel(sortModel);
+
         // Set Optimal Size
         $scope.d.functions.resizeGrid();
 
