d24ed3d4f9fbfdf50f308f7061532275c1ca6936
Beat Ottiger <ottigerb@gmail.com>
2016-02-05 09:00:31 +0100

    wip

--- dass_depressions_angst_stress_skalen/main.js
+++ dass_depressions_angst_stress_skalen/main.js
@@ -214,59 +214,12 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
 
 
         // Init Responses
-        $scope.d.grid = {};
-        $scope.d.grid.rowData = $scope.d.functions.enrichResults(resultsArray);
-
-        // automatic or manually like (columnDefsManually)
-        $scope.d.grid.columnDefs = $scope.d.functions.createColumnDefs($scope.d.grid.rowData, true);
-
-        // columnDefsManually: If you want to create columnDefs manually:
-        // Ref: http://www.angulargrid.com/angular-grid-column-definitions/index.php
-        var columnDefsManually = [{
-            headerTooltip: "Datum",
-            headerName: "Datum",
-            editable: true,
-            suppressSizeToFit: true,
-            width: 145,
-            field: "datestamp",
-            cellClass: 'md-body-1',
-        }, {
-            headerTooltip: "Suchtdruck_1",
-            headerName: "Suchtdruck (Int)",
-            cellClass: 'md-body-2',
-            suppressSizeToFit: true,
-            width: 110,
-            valueGetter: 'parseInt(data.Suchtdruck_1)',
-            filter: 'number'
-        }, {
-            headerName: "Bemerkungen",
-            editable: true,
-            cellClass: 'md-body-1',
-            field: "diary",
-            filter: 'text'
-        }, {
-            headerTooltip: "PID",
-            headerName: "Patient-ID",
-            editable: false,
-            field: "PID",
-            hide: true,
-            cellClass: 'md-body-1',
-            width: 90
-        }, {
-            headerTooltip: "FID",
-            headerName: "Fall-ID",
-            editable: false,
-            field: "FID",
-            hide: true,
-            cellClass: 'md-body-1',
-            width: 90
-        }];
-
 
         // DataView - Options
         $scope.d.grid.options = $scope.d.grid.default_options;
-        $scope.d.grid.options.rowData = $scope.d.grid.rowData;
-        $scope.d.grid.options.columnDefs = $scope.d.grid.columnDefs;
+        $scope.d.grid.options.rowData = $scope.d.functions.enrichResults(resultsArray);
+        $scope.d.grid.options.columnDefs = $scope.d.functions.createColumnDefs($scope.d.grid.rowData, true, true);
+
 
 
         //console.log('dataGRID: ', $scope.d.grid);
