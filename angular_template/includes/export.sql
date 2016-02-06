962a7e4fa52d6d087d51f9f90ab1ac0b1e758c3a
Beat Ottiger <ottigerb@gmail.com>
2016-02-06 12:57:17 +0100

    wip

--- angular_template/includes/export.sql
+++ angular_template/includes/export.sql
@@ -5,6 +5,6 @@ SELECT
   patient.four_letter_code,
   ((cast(response AS json))->>'BSCL[sq504V40]') as sq504V40, 
   recode_into(((cast(response AS json))->>'BSCL[sq504V40]'), '', '-1')  as recode_sq504V40, 
-  recode_into(((cast(response AS json))->>'BSCL[sq504V40]'), '', '0') + 2  as recode_2_sq504V40, 
+  recode_into(((cast(response AS json))->>'BSCL[sq504V40]'), '', '0') + 10  as recode_2_sq504V40 
 FROM survey_response INNER JOIN patient ON(survey_response.patient = patient.id) 
 WHERE module = 'ch.suedhang.apps.bscl.anq'
