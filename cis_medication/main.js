8d2845fb4af1dbf7e3fa53a355fc76dc22bfeb7c
Beat Ottiger <ottigerb@gmail.com>
2016-02-23 23:59:49 +0100

    wip

--- cis_medication/main.js
+++ cis_medication/main.js
@@ -181,7 +181,6 @@ app.controller('AppCtrl', function($scope, $http, $filter, $mdDialog, dataServic
         $scope.d.appState = 'show';
 
         // Set Grid
-        $scope.d._init.grid.grid_ready = false;
         $scope.setDataView($scope.d.app.selected_section.id);
 
         // If Data is loaded - Force the grid to 'pseudo update - data'
