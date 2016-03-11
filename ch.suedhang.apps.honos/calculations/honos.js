0ba352572ffa99ae2c0ebac2cc39043947704503
Beat Ottiger <ottigerb@gmail.com>
2016-03-11 17:09:59 +0100

    this should be fine

--- ch.suedhang.apps.honos/calculations/honos.js
+++ ch.suedhang.apps.honos/calculations/honos.js
@@ -5,9 +5,47 @@ function main(responses) {
     // H e l p e r   -   F U N C T I O N S
     // ------------------------------------------
 
-    calc.doSomething = function() {
-        var score = 73;
-        return score;
+    calc.getType = function(my_value) {
+
+        my_value = parseInt(my_value);
+
+        var type = {
+            name: 'Verlauf',
+            id: my_value
+        };
+
+        if (my_value === 1) {
+            type.name = 'Eintritt';
+        };
+
+        if (my_value === 2) {
+            type.name = 'Austritt';
+        };
+
+        return type;
+    };
+
+    calc.getDropout = function(my_value, my_raeson) {
+
+        my_value = parseInt(my_value);
+
+        var obj = {
+            dropout: false,
+            dropout_raeson: '',
+            dropout_id: my_value
+        };
+
+        if (my_value === 1) {
+            obj.dropout = true;
+            obj.dropout_raeson = 'Patient ist innerhalb von 7 Tagen nach Eintritt ausgetreten.';
+        };
+
+        if (my_value === 2) {
+            obj.dropout = true;
+            obj.dropout_raeson = my_raeson;
+        };
+
+        return obj;
     };
 
 
@@ -23,9 +61,11 @@ function main(responses) {
             var d = {};
             var r = response.data.response;
 
-            // Something
-            d.something = calc.doSomething();
-
+            // --------------------------
+            // Calculation
+            // --------------------------
+            d.type = calc.getType(r['q401V04']);
+            d.dropout = calc.getDropout(r['q401V05'], r['q401V06']);
 
             // Write Results for the Return
             // Do not modify stuff here
