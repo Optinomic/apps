9ab826779845bc869c9317c8f9de68578c764667
Beat Ottiger <ottigerb@gmail.com>
2016-03-11 16:40:37 +0100

    calculation - still works?

--- ch.suedhang.apps.honos/calculations/another_calculation.js
+++ ch.suedhang.apps.honos/calculations/another_calculation.js
@@ -50,6 +50,85 @@ function main(responses) {
 
 
     calc.getSumScores = function(r) {
+        var scores = [{
+            "order": 1,
+            "var": "H1[402V01]",
+            "sum_score": 0
+        }, {
+            "order": 2,
+            "var": "H1[402V02]",
+            "sum_score": 0
+        }, {
+            "order": 3,
+            "var": "H1[402V03]",
+            "sum_score": 0
+        }, {
+            "order": 4,
+            "var": "H1[402V04]",
+            "sum_score": 0
+        }, {
+            "order": 5,
+            "var": "H1[402V05]",
+            "sum_score": 0
+        }, {
+            "order": 6,
+            "var": "H1[402V06]",
+            "sum_score": 0
+        }, {
+            "order": 7,
+            "var": "H1[402V07]",
+            "sum_score": 0
+        }, {
+            "order": 8,
+            "var": "H1[402V08]",
+            "sum_score": 0
+        }, {
+            "order": 9,
+            "var": "H1[402V11]",
+            "sum_score": 0
+        }, {
+            "order": 10,
+            "var": "H1[402V12]",
+            "sum_score": 0
+        }, {
+            "order": 11,
+            "var": "H1[402V13]",
+            "sum_score": 0
+        }, {
+            "order": 12,
+            "var": "H1[402V14]",
+            "sum_score": 0
+        }];
+
+
+        var sum_obj = {
+            "count_kA": 0,
+            "count_value": 0,
+            "sum_score": 0
+        };
+
+        for (var x = 0; x < scores.length; x++) {
+            var current_score = scores[x];
+            var current_value = parseInt(r[current_score.var]);
+
+            if (current_value === 9) {
+                current_score.sum_score = 'k.A.';
+                sum_obj.count_kA = sum_obj.count_kA + 1;
+            } else {
+                current_score.sum_score = current_value;
+                sum_obj.count_value = sum_obj.count_value + 1;
+            };
+
+        };
+
+
+
+        var return_object {
+            total: sum_obj,
+            scores: scores
+        };
+
+        return return_object;
 
     };
 
@@ -71,6 +150,7 @@ function main(responses) {
             // --------------------------------
             d.type = calc.getType(r.q401V04);
             d.dropout = calc.getDropout(r['q401V05'], r['q401V06']);
+            d.sum_scores = getSumScores(r);
 
             d.full_response = myResponses;
 
