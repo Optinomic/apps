ea4a1bdfb662f00ddf5f862331fdee7496bb7115
Beat Ottiger <ottigerb@gmail.com>
2016-03-12 15:40:23 +0100

    note - below

--- ch.suedhang.apps.honos/main.js
+++ ch.suedhang.apps.honos/main.js
@@ -87,75 +87,86 @@ app.controller('AppCtrl', function($scope, dataService, scopeDService) {
         };
         if ($scope.d.dataMain.params.location.viewname === 'honos1_timeline') {
             console.log('imelineChart: honos1_timeline');
-            $scope.d.timeline.options.title = '';
             $scope.d.timeline.name = 'Überaktives, aggressives, Unruhe stiftendes oder agitiertes Verhalten';
             $scope.d.timeline.options.focusField = 'H1[402V01]';
+            $scope.d.timeline.options.title = $scope.d.timeline.options.focusField;
         };
         if ($scope.d.dataMain.params.location.viewname === 'honos2_timeline') {
             console.log('imelineChart: honos1_timeline');
             $scope.d.timeline.options.title = '';
             $scope.d.timeline.name = 'Absichtliche Selbstverletzung';
             $scope.d.timeline.options.focusField = 'H1[402V02]';
+            $scope.d.timeline.options.title = $scope.d.timeline.options.focusField;
         };
         if ($scope.d.dataMain.params.location.viewname === 'honos3_timeline') {
             console.log('imelineChart: honos3_timeline');
             $scope.d.timeline.options.title = '';
             $scope.d.timeline.name = 'Problematischer Alkohol- oder Drogenkonsum';
             $scope.d.timeline.options.focusField = 'H1[402V03]';
+            $scope.d.timeline.options.title = $scope.d.timeline.options.focusField;
         };
         if ($scope.d.dataMain.params.location.viewname === 'honos4_timeline') {
             console.log('imelineChart: honos4_timeline');
             $scope.d.timeline.options.title = '';
             $scope.d.timeline.name = 'Kognitive Probleme';
             $scope.d.timeline.options.focusField = 'H1[402V04]';
+            $scope.d.timeline.options.title = $scope.d.timeline.options.focusField;
         };
         if ($scope.d.dataMain.params.location.viewname === 'honos5_timeline') {
             console.log('imelineChart: honos5_timeline');
             $scope.d.timeline.options.title = '';
             $scope.d.timeline.name = 'Probleme in Zusammenhang mit körperlicher Erkrankung';
             $scope.d.timeline.options.focusField = 'H1[402V05]';
+            $scope.d.timeline.options.title = $scope.d.timeline.options.focusField;
         };
         if ($scope.d.dataMain.params.location.viewname === 'honos6_timeline') {
             console.log('imelineChart: honos6_timeline');
             $scope.d.timeline.options.title = '';
             $scope.d.timeline.name = 'Probleme in Zusammenhang mit Halluzinationen und Wahnvorstellung';
             $scope.d.timeline.options.focusField = 'H1[402V06]';
+            $scope.d.timeline.options.title = $scope.d.timeline.options.focusField;
         };
         if ($scope.d.dataMain.params.location.viewname === 'honos7_timeline') {
             console.log('imelineChart: honos7_timeline');
             $scope.d.timeline.options.title = '';
             $scope.d.timeline.name = 'Gedrückte Stimmung';
             $scope.d.timeline.options.focusField = 'H1[402V07]';
+            $scope.d.timeline.options.title = $scope.d.timeline.options.focusField;
         };
         if ($scope.d.dataMain.params.location.viewname === 'honos8_timeline') {
             console.log('imelineChart: honos8_timeline');
             $scope.d.timeline.options.title = '';
             $scope.d.timeline.name = 'Andere psychische Probleme';
             $scope.d.timeline.options.focusField = 'H1[402V08]';
+            $scope.d.timeline.options.title = $scope.d.timeline.options.focusField;
         };
         if ($scope.d.dataMain.params.location.viewname === 'honos9_timeline') {
             console.log('imelineChart: honos9_timeline');
             $scope.d.timeline.options.title = '';
             $scope.d.timeline.name = 'Probleme mit Beziehung';
             $scope.d.timeline.options.focusField = 'H2[402V11]';
+            $scope.d.timeline.options.title = $scope.d.timeline.options.focusField;
         };
         if ($scope.d.dataMain.params.location.viewname === 'honos10_timeline') {
             console.log('imelineChart: honos10_timeline');
             $scope.d.timeline.options.title = '';
             $scope.d.timeline.name = 'Probleme mit alltäglichen Aktivitäten';
             $scope.d.timeline.options.focusField = 'H2[402V12]';
+            $scope.d.timeline.options.title = $scope.d.timeline.options.focusField;
         };
         if ($scope.d.dataMain.params.location.viewname === 'honos11_timeline') {
             console.log('imelineChart: honos11_timeline');
             $scope.d.timeline.options.title = '';
             $scope.d.timeline.name = 'Probleme durch die Wohnbedingungen';
             $scope.d.timeline.options.focusField = 'H2[402V13]';
+            $scope.d.timeline.options.title = $scope.d.timeline.options.focusField;
         };
         if ($scope.d.dataMain.params.location.viewname === 'honos12_timeline') {
             console.log('imelineChart: honos12_timeline');
             $scope.d.timeline.options.title = '';
             $scope.d.timeline.name = 'Probleme durch die Bedingungen in Beruf und Alltag';
             $scope.d.timeline.options.focusField = 'H2[402V14]';
+            $scope.d.timeline.options.title = $scope.d.timeline.options.focusField;
         };
 
 
