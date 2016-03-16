b6ebc5c82f21afc35f64a71ffbbf438f1ad36bd8
Beat Ottiger <ottigerb@gmail.com>
2016-03-16 13:19:20 +0100

    User-Module:  Erfüllungsgrad

---
+++ com.optinomic.apps.fulfillment/main.js
@@ -0,0 +1,88 @@
+/**
+ * @name Optinomic - AppCtrl
+ * ---------------------------------------
+ * Controller of the Optinomic-Application.
+ */
+app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) {
+
+    // -----------------------------------
+    // Init
+    // -----------------------------------
+
+    // Data-Sharing (do not remove)
+    $scope.d = scopeDService;
+
+
+    // -----------------------------------
+    // Functions
+    // -----------------------------------
+
+    $scope.loadMainData = function() {
+        // -----------------------------------
+        // Get Data: d.dataMain
+        // -----------------------------------
+        $scope.d.haveData = false;
+        var dataPromiseMain = dataService.getMainAppData();
+        dataPromiseMain.then(function(data) {
+
+
+            // Save Data to $scope.d
+            $scope.d.dataMain = data;
+
+            // We always have data
+            $scope.d.haveData = true;
+
+
+            // Check if we have survey_responses @ data.
+            if (data.survey_responses.length !== 0) {
+                console.log('(DATA): survey_responses:', data.survey_responses.length, data.survey_responses);
+
+                // Run App-Functions:
+            };
+
+            // Init - Data Export
+            $scope.setExport();
+
+            // Run Public-Functions:
+            $scope.d.functions.getAllCalculations();
+
+            // Finishing: Console Info & Init = done.
+            console.log('Welcome, ', $scope.d.dataMain.apps.current.name, $scope.d);
+            $scope.d.init = true;
+        });
+    };
+    $scope.loadMainData();
+
+
+
+    // -------------------
+    // Data-Export
+    // -------------------
+    $scope.setExport = function() {
+
+
+        // ------------------------------------------------
+        // Export - Pakete definieren
+        // i n c l u d e _ a s _ j s _ s t r i n g 
+        // => (export.sql) muss sich in /includes befinden
+        // ------------------------------------------------
+
+
+        // Hinzufügen gespeicherter SQL-Dateien in /includes
+        var module_packages = [];
+        // var data_query = {};
+        // data_query = {
+        //     name: 'WHQOL (with stay)',
+        //     sql: in clude_as_js_string(
+        //         WHQOL.sql)
+        // };
+        // module_packages.push(data_query);
+
+
+        $scope.d.sql_box = $scope.d.functions.getDefaultExportSettings($scope.d.dataMain.params.app_id, module_packages);
+
+
+    };
+
+
+});
