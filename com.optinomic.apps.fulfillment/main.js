6a00fb08048936981fa2c37cf3410a67afa00421
Beat Ottiger <ottigerb@gmail.com>
2016-03-16 15:06:34 +0100

    getAppResponses

--- com.optinomic.apps.fulfillment/main.js
+++ com.optinomic.apps.fulfillment/main.js
@@ -33,18 +33,13 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
             $scope.d.haveData = true;
 
 
-            // Check if we have survey_responses @ data.
-            if (data.survey_responses.length !== 0) {
-                console.log('(DATA): survey_responses:', data.survey_responses.length, data.survey_responses);
-
-                // Run App-Functions:
-            };
+            $scope.getAppResponses('ch.suedhang.apps.honos');
 
             // Init - Data Export
             // $scope.setExport();
 
             // Run Public-Functions:
-            $scope.d.functions.getAllCalculations();
+            // $scope.d.functions.getAllCalculations();
 
             // Finishing: Console Info & Init = done.
             console.log('Welcome, ', $scope.d.dataMain.apps.current.name, $scope.d);
@@ -80,6 +75,59 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
 
 
         $scope.d.sql_box = $scope.d.functions.getDefaultExportSettings($scope.d.dataMain.params.app_id, module_packages);
+
+
+    };
+
+
+
+    // -------------------
+    // Get Data
+    // -------------------
+    $scope.getAppResponses = function(app_id) {
+
+        // Init
+        app_id = app_id === undefined ? 'ch.suedhang.apps.honos' : app_id;
+
+        // Querys
+        var app_query = include_as_js_string(
+            responses.sql);
+
+        app_query = app_query.replace("%module_id%", app_id);
+
+
+
+        var sql = {};
+        sql.delimitter = ';';
+        sql.including_headers = 'True';
+        sql.format = 'json';
+        sql.direct = 'True';
+        sql.have_data = false;
+        sql.data = null;
+        sql.packages = [];
+        sql.selectedIndex = 0;
+        sql.query = app_query;
+
+        console.log('(!) getSurveyResponses', sql);
+
+
+        // Get all 'response' fields
+        var api = dataService.runSQL(app_query, sql.delimitter, sql.including_headers, sql.format, sql.direct);
+
+        api.success(function(data) {
+            // var response_json = JSON.parse(data.rows[0].response);
+
+            console.log('(!!) getSurveyResponses', response_json);
+
+
+        });
+
+        api.error(function(data) {
+
+            var text = '(!) Error in SQL (getSurveyResponses)'
+            console.log(text, data);
+            // $scope.d.functions.showSimpleToast(text)
+        });
 
 
     };
