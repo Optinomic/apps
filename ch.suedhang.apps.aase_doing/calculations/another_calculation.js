0d35c5f6304680e7dbe899e7ae0c83226bcea80e
schoenenb <nora.schoenenberger@suedhang.ch>
2016-04-25 17:06:03 +0200

    AASE score

--- ch.suedhang.apps.aase_doing/calculations/another_calculation.js
+++ ch.suedhang.apps.aase_doing/calculations/another_calculation.js
@@ -6,7 +6,28 @@ function main(responses) {
     // ------------------------------------------
 
     calc.doSomething = function() {
-        var score = 73;
+        var score = 0;
+            score = score + parseInt(d['AASE[AASE1]']);
+            score = score + parseInt(d['AASE[AASE2]']);
+            score = score + parseInt(d['AASE[AASE3]']);
+            score = score + parseInt(d['AASE[AASE4]']);
+            score = score + parseInt(d['AASE[AASE5]']);
+            score = score + parseInt(d['AASE[AASE6]']);
+            score = score + parseInt(d['AASE[AASE7]']);
+            score = score + parseInt(d['AASE[AASE8]']);
+            score = score + parseInt(d['AASE[AASE9]']);
+            score = score + parseInt(d['AASE[AASE10]']);
+            score = score + parseInt(d['AASE[AASE11]']);
+            score = score + parseInt(d['AASE[AASE12]']);
+            score = score + parseInt(d['AASE[AASE13]']);
+            score = score + parseInt(d['AASE[AASE14]']);
+            score = score + parseInt(d['AASE[AASE15]']);
+            score = score + parseInt(d['AASE[AASE16]']);
+            score = score + parseInt(d['AASE[AASE17]']);
+            score = score + parseInt(d['AASE[AASE18]']);
+            score = score + parseInt(d['AASE[AASE19]']);
+            score = score + parseInt(d['AASE[AASE20]']);
+
         return score;
     };
 
