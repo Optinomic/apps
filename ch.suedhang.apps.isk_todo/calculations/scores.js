04879c37f3a89addbdf65889ed18cfac6ce3deb4
Beat Ottiger <ottigerb@gmail.com>
2016-04-26 13:44:56 +0200

    FIX 5

--- ch.suedhang.apps.isk_todo/calculations/scores.js
+++ ch.suedhang.apps.isk_todo/calculations/scores.js
@@ -40,6 +40,8 @@ function main(responses) {
 
             // Calculate Stuff
 
+            myResults.result = result;
+
 
             var soz_orient = 0;
             soz_orient = soz_orient + parseInt(result['AISK[AIS1]']);
@@ -67,9 +69,27 @@ function main(responses) {
             myResults.sumscore_offensiv = offensiv;
 
 
+            var selbststeuerung = 0;
+            selbststeuerung = selbststeuerung + parseInt(5 - result['AISK[AIS3]']);
+            selbststeuerung = selbststeuerung + parseInt(5 - result['AISK[AIS7]']);
+            selbststeuerung = selbststeuerung + parseInt(5 - result['AISK[AIS12]']);
+            selbststeuerung = selbststeuerung + parseInt(5 - result['AISK[AIS16]']);
+            selbststeuerung = selbststeuerung + parseInt(result['AISK[AIS20]']);
+            selbststeuerung = selbststeuerung + parseInt(5 - result['AISK[AIS25]']);
+            selbststeuerung = selbststeuerung + parseInt(result['AISK[AIS29]']);
+            selbststeuerung = selbststeuerung + parseInt(5 - result['AISK[AIS33]']);
+            myResults.sumscore_selbststeuerung = selbststeuerung;
 
 
-
+            var reflex = 0;
+            reflex = reflex + parseInt(result['AISK[AIS4]']);
+            reflex = reflex + parseInt(result['AISK[AIS8]']);
+            reflex = reflex + parseInt(result['AISK[AIS13]']);
+            reflex = reflex + parseInt(5 - result['AISK[AIS17]']);
+            reflex = reflex + parseInt(result['AISK[AIS22]']);
+            reflex = reflex + parseInt(result['AISK[AIS26]']);
+            reflex = reflex + parseInt(result['AISK[AIS30]']);
+            myResults.sumscore_reflex = reflex;
 
 
 
