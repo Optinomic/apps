79a0ce78426c4aa0711cb48da762fb52d7904d34
schoenenb <nora.schoenenberger@suedhang.ch>
2016-05-02 16:27:54 +0200

    calc aase v2 with original data

--- ch.suedhang.apps.aase-g/calculations/another_calculation.js
+++ ch.suedhang.apps.aase-g/calculations/another_calculation.js
@@ -5,12 +5,15 @@ function main(responses) {
     // H e l p e r   -   F U N C T I O N S
     // ------------------------------------------
 
-    calc.setScore = function() {
-        var score = 4;
-            score = score + 25;
-        return score;
-    };
+    //calc.setScore = function() {
+    //    var score = 4;
+    //        score = score + 25;
+    //    return score;
+    //};
 
+    calc.roundToOne = function(num) {
+        return +(Math.round(num + "e+1") + "e-1");
+    };
 
     // ------------------------------------------
     // F U N C T I O N  -  Main
@@ -24,8 +27,34 @@ function main(responses) {
             var myResults = {};
             var result = response.data.response;
 
+
+            //Score-Berechnung
+            var score = 0;
+            score = score + parseInt(result['AASE[AASE1]']);
+            score = score + parseInt(result['AASE[AASE2]']);
+            score = score + parseInt(result['AASE[AASE3]']);
+            score = score + parseInt(result['AASE[AASE4]']);
+            score = score + parseInt(result['AASE[AASE5]']);
+            score = score + parseInt(result['AASE[AASE6]']);
+            score = score + parseInt(result['AASE[AASE7]']);
+            score = score + parseInt(result['AASE[AASE8]']);
+            score = score + parseInt(result['AASE[AASE9]']);
+            score = score + parseInt(result['AASE[AASE10]']);
+            score = score + parseInt(result['AASE[AASE11]']);
+            score = score + parseInt(result['AASE[AASE12]']);
+            score = score + parseInt(result['AASE[AASE13]']);
+            score = score + parseInt(result['AASE[AASE14]']);
+            score = score + parseInt(result['AASE[AASE15]']);
+            score = score + parseInt(result['AASE[AASE16]']);
+            score = score + parseInt(result['AASE[AASE17]']);
+            score = score + parseInt(result['AASE[AASE18]']);
+            score = score + parseInt(result['AASE[AASE19]']);
+            score = score + parseInt(result['AASE[AASE20]']);
+            
+            myResults.score = calc.roundToOne(score)
+
             // Get score
-            myResults.getScore = calc.setScore();
+            //myResults.getScore = calc.setScore();
 
 
             // Write Results for the Return
