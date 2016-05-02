bb9568b461a67b6e989d84a1f2ce29fcdbf87622
schoenenb <nora.schoenenberger@suedhang.ch>
2016-05-02 11:26:12 +0200

    calculation AASE
    d[] --> result[]

--- ch.suedhang.apps.aase_doing/calculations/another_calculation.js
+++ ch.suedhang.apps.aase_doing/calculations/another_calculation.js
@@ -5,28 +5,28 @@ function main(responses) {
     // H e l p e r   -   F U N C T I O N S
     // ------------------------------------------
 
-    calc.doSomething = function() {
+    calc.getScore = function() {
         var score = 0;
-            score = score + parseInt(d['AASE[AASE1]']);
-            score = score + parseInt(d['AASE[AASE2]']);
-            score = score + parseInt(d['AASE[AASE3]']);
-            score = score + parseInt(d['AASE[AASE4]']);
-            score = score + parseInt(d['AASE[AASE5]']);
-            score = score + parseInt(d['AASE[AASE6]']);
-            score = score + parseInt(d['AASE[AASE7]']);
-            score = score + parseInt(d['AASE[AASE8]']);
-            score = score + parseInt(d['AASE[AASE9]']);
-            score = score + parseInt(d['AASE[AASE10]']);
-            score = score + parseInt(d['AASE[AASE11]']);
-            score = score + parseInt(d['AASE[AASE12]']);
-            score = score + parseInt(d['AASE[AASE13]']);
-            score = score + parseInt(d['AASE[AASE14]']);
-            score = score + parseInt(d['AASE[AASE15]']);
-            score = score + parseInt(d['AASE[AASE16]']);
-            score = score + parseInt(d['AASE[AASE17]']);
-            score = score + parseInt(d['AASE[AASE18]']);
-            score = score + parseInt(d['AASE[AASE19]']);
-            score = score + parseInt(d['AASE[AASE20]']);
+            score = score + parseInt(result['AASE[AASE1]']);
+            score = score + parseInt(result['AASE[AASE2]']);
+            score = score + parseInt(result['AASE[AASE3]']);
+            score = score + parseInt(result['AASE[AASE4]']);
+            score = score + parseInt(result['AASE[AASE5]']);
+            score = score + parseInt(result['AASE[AASE6]']);
+            score = score + parseInt(result['AASE[AASE7]']);
+            score = score + parseInt(result['AASE[AASE8]']);
+            score = score + parseInt(result['AASE[AASE9]']);
+            score = score + parseInt(result['AASE[AASE10]']);
+            score = score + parseInt(result['AASE[AASE11]']);
+            score = score + parseInt(result['AASE[AASE12]']);
+            score = score + parseInt(result['AASE[AASE13]']);
+            score = score + parseInt(result['AASE[AASE14]']);
+            score = score + parseInt(result['AASE[AASE15]']);
+            score = score + parseInt(result['AASE[AASE16]']);
+            score = score + parseInt(result['AASE[AASE17]']);
+            score = score + parseInt(result['AASE[AASE18]']);
+            score = score + parseInt(result['AASE[AASE19]']);
+            score = score + parseInt(result['AASE[AASE20]']);
 
         return score;
     };
@@ -45,7 +45,7 @@ function main(responses) {
             var result = response.data.response;
 
             // Something
-            myResults.something = calc.doSomething();
+            myResults.something = calc.getScore();
 
 
             // Write Results for the Return
