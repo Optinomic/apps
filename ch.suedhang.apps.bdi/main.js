dae1e86186b2cce76681e19964232fdc4ca0a74b
Beat Ottiger <ottigerb@gmail.com>
2016-05-04 12:20:39 +0200

    BDI : Improvements 4

--- ch.suedhang.apps.bdi/main.js
+++ ch.suedhang.apps.bdi/main.js
@@ -70,37 +70,11 @@ app.controller('AppCtrl', function($scope, dataService, scopeDService) {
         //console.log('setCurrentResultDate', $scope.d.dataMain.calculations[0].calculation_results[$scope.d.navigator].response.data);
     };
 
+
     $scope.bdi_init = function() {
 
-        // Ranges initialisieren
-        $scope.scale_ranges = {
-            "ranges": [{
-                "from": 0,
-                "to": 8,
-                "result": "Keine Depression",
-                "result_color": "green"
-            }, {
-                "from": 9,
-                "to": 13,
-                "result": "Minimale Depression",
-                "result_color": "green"
-            }, {
-                "from": 14,
-                "to": 19,
-                "result": "Leichte Depression",
-                "result_color": "orange"
-            }, {
-                "from": 20,
-                "to": 28,
-                "result": "Mittelschwere Depression",
-                "result_color": "orange"
-            }, {
-                "from": 29,
-                "to": 63,
-                "result": "Schwere Depression",
-                "result_color": "red"
-            }]
-        };
+        // Ranges initialisieren aus 'Calculation'
+        $scope.scale_ranges = $scope.d.dataMain.calculations[0].calculation_results[d.navigator].score.ranges;
     };
 
 
