0913060cdbd6139ea32779a8bff3f41de3c2e448
Beat Ottiger <ottigerb@gmail.com>
2016-05-04 09:47:49 +0200

    AASE-G ::  Erhebungszeitpunkt - FIX

--- ch.suedhang.apps.aase-g/main.js
+++ ch.suedhang.apps.aase-g/main.js
@@ -3,7 +3,7 @@
  * ---------------------------------------
  * Controller of the Optinomic-Application.
  */
-app.controller('AppCtrl', function($scope, dataService, scopeDService) {
+app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) {
 
     // -----------------------------------
     // Init
