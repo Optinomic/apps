f84207c1b0f38687dc66cf36ce7a6991f5ee533a
schoenenb <nora.schoenenberger@suedhang.ch>
2016-05-23 17:34:28 +0200

    tmt export

--- ch.suedhang.apps.tmt/includes/export.sql
+++ ch.suedhang.apps.tmt/includes/export.sql
@@ -1,12 +1,50 @@
 SELECT
-  sr.id as survey_response_id, 
-  sr.response, 
-  sr.event_id, 
-  sr.filled, 
-  ev.patient as patient_id,
-  first(s.id) as stay_id
+  patient.id AS pid,
+  patient,
+  CASE WHEN patient.gender='Male' THEN 'Herr' ELSE 'Frau' END || ' ' || COALESCE(patient.last_name, '') || ' ' || COALESCE(patient.first_name, '') AS patient_name,
+  patient.four_letter_code,
 
-FROM survey_responses as sr 
-LEFT JOIN event as ev on ev.id = sr.event_id 
-LEFT JOIN stay as s on s.patient = ev.patient and s.start <= ev.created_at and (s.stop >= ev.created_at or s.stop is null) 
-GROUP BY sr.id, sr.response, sr.event_id, sr.filled, ev.patient
+  ((cast(response AS json))->>'Alter') as Alter,
+  ((cast(response AS json))->>'Ausbildungsjahre') as Ausbildungsjahre
+  ((cast(response AS json))->>'TMTATime') as ZeitA,
+  ((cast(response AS json))->>'TMTAError') as FehlerA,
+  ((cast(response AS json))->>'AzWert') as zWertA,
+  ((cast(response AS json))->>'TMTBTime') as ZeitB,
+  ((cast(response AS json))->>'TMTBError') as FehlerB,
+  ((cast(response AS json))->>'BzWert') as zWertB,
+  ((cast(response AS json))->>'BAzWert') as zWertQ,
+  ((cast(response AS json))->>'filled') as filled,
+  ((cast(response AS json))->>'Date') as Datum,
+  TO_DATE(((cast(response AS json))->>'Date'), 'YYYY-MM-DD HH24:MI:SS')  as datum_date,
+  SUBSTRING(((cast(response AS json))->>'Date'),12,5) AS datum_time,
+  SUBSTRING(((cast(response AS json))->>'Date'),1,4)::integer AS datum_year,
+  EXTRACT(WEEK FROM TO_DATE(((cast(response AS json))->>'Date'), 'YYYY-MM-DD HH24:MI:SS')) AS datum_week,
+  ((cast(response AS json))->>'Messzeitpunkt') as erhebungszeitpunkt,
+  ((cast(response AS json))->>'FID') as fid,
+  ((cast(response AS json))->>'PID') as pid,
+  ((cast(response AS json))->>'datestamp') as datestamp,
+  TO_DATE(((cast(response AS json))->>'datestamp'), 'YYYY-MM-DD HH24:MI:SS')  as datestamp_date,
+  SUBSTRING(((cast(response AS json))->>'datestamp'),12,5) AS datestamp_time,
+  SUBSTRING(((cast(response AS json))->>'datestamp'),1,4)::integer AS datestamp_year,
+  EXTRACT(WEEK FROM TO_DATE(((cast(response AS json))->>'datestamp'), 'YYYY-MM-DD HH24:MI:SS')) AS datestamp_week,
+  ((cast(response AS json))->>'id') as id,
+  ((cast(response AS json))->>'lastpage') as lastpage,
+  ((cast(response AS json))->>'optinomixHASH') as optinomixhash,
+  ((cast(response AS json))->>'startdate') as startdate,
+  TO_DATE(((cast(response AS json))->>'startdate'), 'YYYY-MM-DD HH24:MI:SS')  as startdate_date,
+  SUBSTRING(((cast(response AS json))->>'startdate'),12,5) AS startdate_time,
+  SUBSTRING(((cast(response AS json))->>'startdate'),1,4)::integer AS startdate_year,
+  EXTRACT(WEEK FROM TO_DATE(((cast(response AS json))->>'startdate'), 'YYYY-MM-DD HH24:MI:SS')) AS startdate_week,
+  ((cast(response AS json))->>'startlanguage') as startlanguage,
+  ((cast(response AS json))->>'submitdate') as submitdate,
+
+  random_hash,
+  scheduled,
+  filled,
+  module,
+  survey_response.id AS survey_response_id  
+
+FROM survey_response 
+INNER JOIN patient ON(survey_response.patient = patient.id) 
+
+WHERE module = 'ch.suedhang.apps.tmt_new';
