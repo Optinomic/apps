8603520291da0711b4e26be781e5e6ce989705fd
Beat Ottiger <ottigerb@gmail.com>
2016-05-30 14:53:20 +0200

    SQL

--- com.optinomic.init.stay/includes/select_test.sql
+++ com.optinomic.init.stay/includes/select_test.sql
@@ -1,62 +1,26 @@
 
-1.)
 SELECT 
-*, 
-cast(value as json) AS json
-FROM annotations r
-
-
-2.)
-SELECT 
-*, 
-((cast(value AS json))->>'init_stay') as fnr,
-cast(value as json) AS json
-FROM annotations r
-WHERE module = 'com.optinomic.init.stay'
-
-
-3.) 
-
-SELECT *
-FROM   json_array_elements(
-  '[{"name": "Toby", "occupation": "Software Engineer"},
-    {"name": "Zaphod", "occupation": "Galactic President"} ]'
-  ) AS elem
-WHERE elem->>'name' = 'Toby';
-
-
-4.)
-
-https://www.postgresql.org/docs/current/static/functions-json.html#FUNCTIONS-JSON-TABLE
-
-
-SELECT 
-*, 
-((cast(value AS json))->>'init_stay') as fnr,
-((cast(value AS json))->>'init_stay')::json#>'2' as fnr,
-cast(value as json) AS json
-FROM annotations r
-WHERE module = 'com.optinomic.init.stay'
-
-
-5.)  Working - Nested Object
-
-SELECT 
-
-((cast(value AS json))->'patient_group_selector'->>'EAS') as EAS
-
+((cast(value AS json))->'patient_group_selector'->>'current_eas') as EAS
 FROM annotations
 WHERE module = 'com.optinomic.init.stay' 
 
 
-
+// Currently in EAS
 
 
 SELECT 
-*
+patient_id as pid, *
 FROM annotations
 WHERE module = 'com.optinomic.init.stay' 
-AND ((cast(value AS json))->'patient_group_selector'->>'EAS') = 'true'
+AND ((cast(value AS json))->'patient_group_selector'->>'current_eas') = 'true'
 
+
+// Currently in EAS or had EAS
+
+SELECT 
+patient_id as pid, *
+FROM annotations
+WHERE module = 'com.optinomic.init.stay' 
+AND ((((cast(value AS json))->'patient_group_selector'->>'current_eas') = 'true') OR  (((cast(value AS json))->'patient_group_selector'->>'past_eas') = 'true'))
 
 
