c240f3c5b31913ae6c904b9ce39ccf2dfb49e257
Beat Ottiger <ottigerb@gmail.com>
2016-05-30 10:35:03 +0200

    wip

--- com.optinomic.init.stay/main.js
+++ com.optinomic.init.stay/main.js
@@ -267,16 +267,19 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
         // Get History - Array
         // -------------------------------------
 
-        var api_call = dataService.getAnnotationsData('patient', 'history');
-        api_call.then(function(data) {
 
+        var api = dataService.getPatientModuleAnnotations();
+
+        var aPromise = dataService.getData(api);
+        aPromise.then(function(data) {
 
             if (dataService.isEmpty(data)) {
                 console.log('getInit = Empty');
             } else {
                 console.log('getInit = ', data);
 
-                $scope.d.init_stay.history_states = angular.copy(data);
+                $scope.d.init_stay.history_states = angular.copy(data.history);
+                $scope.d.init_stay.patient_group_selector = angular.copy(data.patient_group_selector);
 
                 $scope.d.init_stay.history_states.forEach(function(item, myindex) {
 
@@ -291,13 +294,15 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
                 });
             };
 
+            deferred.resolve(return_data);
 
-            // Set Default
-            $scope.changeTreatment();
-
-            //console.log('(✓) getInit: ', $scope.d.init_stay.history_states);
-
+        }, function(error) {
+            // Error
+            deferred.reject(error);
+            console.log('ERROR: savePatientGroupObject', error);
         });
+
+
     };
 
 
