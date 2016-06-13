e49a21ad7545617aead0e5707d5a93fb3b67a365
schoenenb <nora.schoenenberger@suedhang.ch>
2016-06-13 08:56:57 +0200

    CASE calc

--- ch.suedhang.apps.case/calculations/another_calculation.js
+++ ch.suedhang.apps.case/calculations/another_calculation.js
@@ -62,38 +62,30 @@ function main(responses) {
 
             //Score-Berechnung
             var score = 0;
-            score = score + parseInt(result['Abbrueche']);
-            score = score + parseInt(result['AbstMot']);
-            score = score + parseInt(result['Allein']);
-            score = score + parseInt(result['Alter']);
-            score = score + parseInt(result['Arbeitslos']);
-            score = score + parseInt(result['Craving']);
             score = score + parseInt(result['DauerAbh']);
-            score = score + parseInt(result['Kontrollv']);
+            score = score + parseInt(result['statEnzug']);
+            score = score + parseInt(result['ambEntzugMedi']);
+            score = score + parseInt(result['Craving']);
+            score = score + parseInt(result['statEntwoehnung']);
+            score = score + parseInt(result['ambEntwoehnung']);
+            score = score + parseInt(result['Abbrueche']);
+            score = score + parseInt(result['Rueckfaelle']);
             score = score + parseInt(result['Leber']);
-            score = score + parseInt(result['MorgenTr']);
+            score = score + parseInt(result['Somat']);
             score = score + parseInt(result['Neurol']);
             score = score + parseInt(result['Psych']);
-            score = score + parseInt(result['Rueckfaelle']);
-            score = score + parseInt(result['Somat']);
+            score = score + parseInt(result['Arbeitslos']);
             score = score + parseInt(result['Wohnen']);
-            score = score + parseInt(result['ambEntwoehnung']);
-            score = score + parseInt(result['ambEntzugMedi']);
+            score = score + parseInt(result['Allein']);
+            score = score + parseInt(result['Alter']);
+            score = score + parseInt(result['Kontrollv']);
+            score = score + parseInt(result['MorgenTr']);
             score = score + parseInt(result['posErw']);
-            score = score + parseInt(result['statEntwoehnung']);
-            score = score + parseInt(result['statEnzug']);
-            
+            score = score + parseInt(result['AbstMot']);
+
+       
             myResults.score = calc.roundToOne(score)
 
-            // Get score
-            //myResults.getScore = calc.setScore();
-
-
-            // Write Results for the Return
-            // Do not modify stuff here
-            myResults.hash = result['optinomixHASH'];
-            myResults.response = response;
-            allResults.push(myResults);
         });
 
         return allResults;
