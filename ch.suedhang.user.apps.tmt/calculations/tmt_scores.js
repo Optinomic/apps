cc7d975ce7c81eaddb3aae8b674d34019fbf6eb9
Beat Ottiger <ottigerb@gmail.com>
2016-08-22 17:40:16 +0200

    hmmm?

--- ch.suedhang.user.apps.tmt/calculations/tmt_scores.js
+++ ch.suedhang.user.apps.tmt/calculations/tmt_scores.js
@@ -74,7 +74,7 @@ function main(responses) {
                 var event_id = current_response.response.data.event_id;
                 var pid = current_result.patient.id;
 
-                var mz = current_response.mz;
+                // var mz = current_response.mz;
 
 
                 // Details Obj. erstellen.
@@ -86,7 +86,7 @@ function main(responses) {
                     "Perz_A": Perz_A,
                     "Perz_B": Perz_B,
                     "BA_Quotient": BA_Quotient,
-                    "Messzeitpunkt": mz,
+                    //"Messzeitpunkt": mz,
                     "full_response": current_response,
                     "event_id": event_id,
                     "patient_id": pid,
