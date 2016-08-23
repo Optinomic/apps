5a2fbf0167a84fa746fa4c9fb3693e514996eb8c
Beat Ottiger <ottigerb@gmail.com>
2016-08-23 16:33:21 +0200

    wip

--- ch.suedhang.user.apps.tmt/main.js
+++ ch.suedhang.user.apps.tmt/main.js
@@ -518,14 +518,26 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
             };
 
 
-            // var safe_here = age_edu_groups[age_group].[edu_id];
-            // safe_here.TMTAError = 99;
+            //  // Safe in given 'edu_id'
+            var safe_here = age_edu_groups[age_group].[edu_id];
 
+            safe_here.mz_eintritt = safe_here.mz_eintritt.concat(current_result.mz_eintritt_vars);
+            safe_here.mz_austritt = safe_here.mz_austritt.concat(current_result.mz_austritt_vars);
+            safe_here.mz_anderer = safe_here.mz_anderer.concat(current_result.mz_anderer_vars);
+            safe_here.mz_alle = safe_here.mz_alle.concat(current_result.mz_alle_vars);
+
+
+            // Safe also in edu_all
+            safe_here = age_edu_groups[age_group].edu_all;
+
+            safe_here.mz_eintritt = safe_here.mz_eintritt.concat(current_result.mz_eintritt_vars);
+            safe_here.mz_austritt = safe_here.mz_austritt.concat(current_result.mz_austritt_vars);
+            safe_here.mz_anderer = safe_here.mz_anderer.concat(current_result.mz_anderer_vars);
+            safe_here.mz_alle = safe_here.mz_alle.concat(current_result.mz_alle_vars);
 
         };
 
         return age_edu_groups;
-
     };
 
 
