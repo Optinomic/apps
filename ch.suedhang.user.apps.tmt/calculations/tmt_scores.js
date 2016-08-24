5c07775727074ac4fabcda452fa0b2d75a1a2f22
Beat Ottiger <ottigerb@gmail.com>
2016-08-24 15:13:06 +0200

    loop & test write

--- ch.suedhang.user.apps.tmt/calculations/tmt_scores.js
+++ ch.suedhang.user.apps.tmt/calculations/tmt_scores.js
@@ -394,16 +394,19 @@ include(../lib/js/optinomic/statistics/calculation_simplestatistics.js)
         var edu_props = ['edu_all', 'edu_high', 'edu_small'];
         var mz_props = ['mz_eintritt', 'mz_austritt', 'mz_anderer', 'mz_alle'];
 
+        for (var group_id = 0; group_id < 11; group_id++) {
+            
+            for (var edu_prop_id = 0; edu_prop_id < edu_props.length; edu_prop_id++) {
+                var edu_prop = edu_props[edu_prop_id];
 
-        for (var edu_prop_id = 0; edu_prop_id < edu_props.length; edu_prop_id++) {
-            var edu_prop = edu_props[edu_prop_id];
+                for (var mz_prop_id = 0; mz_prop_id < mz_props.length; mz_prop_id++) {
+                    var mz_prop = mz_props[mz_prop_id];
 
-            for (var mz_prop_id = 0; mz_prop_id < mz_props.length; mz_prop_id++) {
-                var mz_prop = mz_props[mz_prop_id];
-    
-    
+                    //Writing
+                    statistics_age_edu_groups[group_id][edu_prop][mz_prop].statistics = 73;
+                };
+
             };
-
         };
 
         return statistics_age_edu_groups;
