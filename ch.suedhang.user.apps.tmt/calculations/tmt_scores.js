a2353169fde5407afaabdeb2ab86658eec7a64d6
Beat Ottiger <ottigerb@gmail.com>
2016-08-27 14:11:22 +0200

    hopefully fixed

--- ch.suedhang.user.apps.tmt/calculations/tmt_scores.js
+++ ch.suedhang.user.apps.tmt/calculations/tmt_scores.js
@@ -154,20 +154,20 @@ function main(responses) {
         // var mz_props = calc.group_mz_props;
 
 
-        for (var group_id = 0; group_id < age_props.length; group_id++) {
+        for (var group_id = 0; group_id < calc.group_age_props.length; group_id++) {
 
             var inner_obj = {};
-            inner_obj.edu_group_array_id = group_id;
+            inner_obj.age_group_array_id = group_id;
             //inner_obj = calc.collect(calc.group_age_props[group_id]);
 
-            for (var edu_prop_id = 0; edu_prop_id < edu_props.length; edu_prop_id++) {
-                inner_obj.info.education = edu_prop_id;
+            for (var edu_prop_id = 0; edu_prop_id < calc.group_edu_props.length; edu_prop_id++) {
 
-                for (var mz_prop_id = 0; mz_prop_id < mz_props.length; mz_prop_id++) {
+                inner_obj.edu_group_array_id = edu_prop_id;
 
-                    inner_obj.info.mz = mz_prop_id;
+                for (var mz_prop_id = 0; mz_prop_id < calc.group_mz_props.length; mz_prop_id++) {
 
-
+                    inner_obj.mz_group_array_id = mz_prop_id;
+                    
                     obj_name = 'age_' + twoDigits(group_id) + '_edu_' + twoDigits(edu_prop_id) + '_mz_' + twoDigits(mz_prop_id);
 
                     // Write to Object
