a32f4a0300284f811eb39681e1a68d0331ff0cba
Beat Ottiger <ottigerb@gmail.com>
2016-09-13 21:52:06 +0200

    this is still fine - right?

--- ch.suedhang.user.apps.tmt/calculations/tmt_samples.js
+++ ch.suedhang.user.apps.tmt/calculations/tmt_samples.js
@@ -289,9 +289,9 @@ function main(responses) {
             var current_patient_score = patient_scores[x];
 
             // Read given ArrayPositions | Infos
-            // var md_pos_1_id = current_patient_score.info.group_age_array_id;
-            // var md_pos_2_id = current_patient_score.info.group_edu_array_id;
-            // var md_pos_3_id = current_patient_score.info.group_mz_array_id;
+            var md_pos_1_id = current_patient_score.info.group_age_array_id;
+            var md_pos_2_id = current_patient_score.info.group_edu_array_id;
+            var md_pos_3_id = current_patient_score.info.group_mz_array_id;
             // 
             // var md_pos_1_info = md_info[md_pos_1_id];
             // var md_pos_2_info = md_info[md_pos_3_id];
