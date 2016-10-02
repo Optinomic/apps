e25cbaa0db21bb03a6e8084c92a30fcf0060c9b0
Beat Ottiger <ottigerb@gmail.com>
2016-10-02 23:32:55 +0200

    app_dimensions

--- ch.suedhang.user.apps.tmt/calculations/klinikstichprobe.js
+++ ch.suedhang.user.apps.tmt/calculations/klinikstichprobe.js
@@ -20,6 +20,95 @@ function main(responses) {
         "TMTBZ": []
     };
 
+    // ------------------------------------------
+    // What 'Groups' do we have?
+    // ------------------------------------------
+
+    calc.group_age_props = [{
+        "id": 0,
+        "text": "Altersgruppe 18 - 24"
+    }, {
+        "id": 1,
+        "text": "Altersgruppe 25 – 34"
+    }, {
+        "id": 2,
+        "text": "Altersgruppe 35 – 44"
+    }, {
+        "id": 3,
+        "text": "Altersgruppe 45 – 54"
+    }, {
+        "id": 4,
+        "text": "Altersgruppe 55 – 59"
+    }, {
+        "id": 5,
+        "text": "Altersgruppe 60 – 64"
+    }, {
+        "id": 6,
+        "text": "Altersgruppe 65 – 69"
+    }, {
+        "id": 7,
+        "text": "Altersgruppe 70 – 74"
+    }, {
+        "id": 8,
+        "text": "Altersgruppe 75 – 79"
+    }, {
+        "id": 9,
+        "text": "Altersgruppe 80 – 84"
+    }, {
+        "id": 10,
+        "text": "Altersgruppe 85 – 89"
+    }];
+
+
+    calc.group_edu_props = [{
+        "id": 0,
+        "text": "Ausbildung: <= 12 Jahre"
+    }, {
+        "id": 1,
+        "text": "Ausbildung: > 12 Jahre"
+    }, {
+        "id": 99,
+        "text": "Ausbildung: Alle Levels"
+    }];
+
+
+    calc.group_mz_props = [{
+        "id": 0,
+        "text": "Messzeitpunkt: Eintritt",
+        "survey_id": 1
+    }, {
+        "id": 1,
+        "text": "Messzeitpunkt: Austritt",
+        "survey_id": 2
+    }, {
+        "id": 2,
+        "text": "Messzeitpunkt: Anderer",
+        "survey_id": 3
+    }, {
+        "id": 3,
+        "text": "Alle Messzeitpunkte",
+        "survey_id": null
+    }];
+
+
+    // ------------------------------------------
+    // What Dimensions are given from App
+    // Arrange them in the following array
+    // ------------------------------------------
+
+    calc.dimensions_app = [{
+        "id": 0,
+        "name": "Altersgruppe",
+        "definitions": calc.cloneObj(group_age_props)
+    }, {
+        "id": 1,
+        "name": "Ausbildungsstand",
+        "definitions": calc.cloneObj(group_edu_props)
+    }, {
+        "id": 2,
+        "name": "Messzeitpunkt",
+        "definitions": calc.cloneObj(group_mz_props)
+    }];
 
     // ------------------------------------------
     // Info: com.optinomic.user.apps.klinikstichproben
@@ -113,6 +202,11 @@ function main(responses) {
         var info = calc.cloneObj(calc.info);
         info.other_calculation = info.patient_app_id + ':' + info.patient_app_calculation;
 
+        var group_age_props = calc.cloneObj(calc.group_age_props);
+        var group_edu_props = calc.cloneObj(calc.group_edu_props);
+        var group_mz_props = calc.cloneObj(calc.group_mz_props);
+        var dimensions_app = calc.cloneObj(calc.dimensions_app);
+
         // Arrange Stuff as 'variables'
         var patient_scores = calc.getScoresInVars(d.patients, vars, info);
 
@@ -122,6 +216,9 @@ function main(responses) {
 
         results.info = info;
         results.variables = vars;
+        results.dimensions_app = dimensions_app;
+
+
 
         // Returning full (complete) responses is often used/helpful.
         results.full = d;
