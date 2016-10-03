abc4319727ac1e30922b98e54e182c8e11fda5b2
Beat Ottiger <ottigerb@gmail.com>
2016-10-03 19:39:00 +0200

    wip

--- ch.suedhang.user.apps.tmt/calculations/klinikstichprobe.js
+++ ch.suedhang.user.apps.tmt/calculations/klinikstichprobe.js
@@ -316,7 +316,7 @@ function main(responses) {
 
                 var my_ziel = ziel[current_var];
                 var my_quelle = quelle[current_var];
-                ziel[current_var] = my_ziel.concat(my_quelle);
+                //ziel[current_var] = my_ziel.concat(my_quelle);
             };
 
             ziel.patients.push(patient);
