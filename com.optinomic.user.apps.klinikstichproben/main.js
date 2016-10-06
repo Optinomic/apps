f37d593c75cf8321097ba7852d618618318a9464
Beat Ottiger <ottigerb@gmail.com>
2016-10-06 11:14:33 +0200

    hmm...

--- com.optinomic.user.apps.klinikstichproben/main.js
+++ com.optinomic.user.apps.klinikstichproben/main.js
@@ -376,7 +376,7 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
                     // Test Write
                     var ziel = data_dive;
                     data_dive[0][0] = concatAndStatistics(ziel, current_score, pid, vars_array);
-                    data[0][0][0][0][0] = concatAndStatistics(ziel, current_score, pid, vars_array);
+                    //data[0][0][0][0][0] = concatAndStatistics(ziel, current_score, pid, vars_array);
 
                     // In allen Varianten schreiben.
 
