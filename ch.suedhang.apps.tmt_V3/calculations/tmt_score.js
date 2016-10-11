0a2594bf348d1da1a975e2fae7218bbfe1bb5d55
Beat Ottiger <ottigerb@gmail.com>
2016-10-11 11:13:04 +0200

    TMT ::  Merge different 'surveys'

--- ch.suedhang.apps.tmt_V3/calculations/tmt_score.js
+++ ch.suedhang.apps.tmt_V3/calculations/tmt_score.js
@@ -622,70 +622,77 @@ function main(responses) {
             var result = response.data.response;
 
 
-            if (result.survey_version === 'ng_survey_v1') {
-
-                // Age & Edu
-                myResults.birthdate = d.patient.data.birthdate
-
-                var set_age = calc.getPatientAgeMz(d.patient.data.birthdate, response.data.filled);
-                myResults.set_age = set_age;
-                myResults.date = response.data.filled;
-
-                var edu_years = calc.roundToTwo(result.edu_years);
-                myResults.edu_years = edu_years;
-
-
-                // Zeit & Fehler in Integer
-                myResults.TMTAError = parseInt(result.tmt_a_error);
-                myResults.TMTBError = parseInt(result.tmt_b_error);
-                myResults.TMTATime = parseInt(result.tmt_a_time);
-                myResults.TMTBTime = parseInt(result.tmt_b_time);
-
-
-                // Calculate Stuff
-                myResults.quotient = calc.quotient(result);
-                myResults.quotient_rounded = calc.roundToTwo(calc.quotient(result));
-                myResults.percentile = calc.get_percentile(result, set_age, edu_years);
-
-                // Messzeitpunkt
-                var Messzeitpunkt = parseInt(result.mz);
-                myResults.mz = Messzeitpunkt;
-
-                var Messzeitpunkt_Text = 'Undefined';
-
-                if (Messzeitpunkt === 1) {
-                    Messzeitpunkt_Text = 'Eintritt';
-                };
-                if (Messzeitpunkt === 2) {
-                    Messzeitpunkt_Text = 'Austritt';
-                };
-                if (Messzeitpunkt === 3) {
-                    Messzeitpunkt_Text = 'Anderer Messzeitpunkt';
-                };
-
-                var mz_obj = {
-                    "Messzeitpunkt": Messzeitpunkt,
-                    "Messzeitpunkt_Text": Messzeitpunkt_Text
-                };
-
-                myResults.Messzeitpunkt = mz_obj;
-
-                // Write Results for the Return
-                // Do not modify stuff here
-                myResults.hash = result['optinomixHASH'];
-                myResults.response = response;
-                // myResults.d = d;
-
-                allResults.push(myResults);
-
+            if (result.survey_version !== 'ng_survey_v1') {
+                result.tmt_a_error = result.TMTAError;
+                result.tmt_a_time = result.TMTATime;
+                result.tmt_b_error = result.TMTBError;
+                result.tmt_b_time = result.TMTBTime;
+                result.edu_years = result.Ausbildungsjahre;
+                result.mz = result.Messzeitpunkt;
             };
+
+
+            // Age & Edu
+            myResults.birthdate = d.patient.data.birthdate
+
+            var set_age = calc.getPatientAgeMz(d.patient.data.birthdate, response.data.filled);
+            myResults.set_age = set_age;
+            myResults.date = response.data.filled;
+
+            var edu_years = calc.roundToTwo(result.edu_years);
+            myResults.edu_years = edu_years;
+
+
+            // Zeit & Fehler in Integer
+            myResults.TMTAError = parseInt(result.tmt_a_error);
+            myResults.TMTBError = parseInt(result.tmt_b_error);
+            myResults.TMTATime = parseInt(result.tmt_a_time);
+            myResults.TMTBTime = parseInt(result.tmt_b_time);
+
+
+            // Calculate Stuff
+            myResults.quotient = calc.quotient(result);
+            myResults.quotient_rounded = calc.roundToTwo(calc.quotient(result));
+            myResults.percentile = calc.get_percentile(result, set_age, edu_years);
+
+            // Messzeitpunkt
+            var Messzeitpunkt = parseInt(result.mz);
+            myResults.mz = Messzeitpunkt;
+
+            var Messzeitpunkt_Text = 'Undefined';
+
+            if (Messzeitpunkt === 1) {
+                Messzeitpunkt_Text = 'Eintritt';
+            };
+            if (Messzeitpunkt === 2) {
+                Messzeitpunkt_Text = 'Austritt';
+            };
+            if (Messzeitpunkt === 3) {
+                Messzeitpunkt_Text = 'Anderer Messzeitpunkt';
+            };
+
+            var mz_obj = {
+                "Messzeitpunkt": Messzeitpunkt,
+                "Messzeitpunkt_Text": Messzeitpunkt_Text
+            };
+
+            myResults.Messzeitpunkt = mz_obj;
+
+            // Write Results for the Return
+            // Do not modify stuff here
+            myResults.hash = result['optinomixHASH'];
+            myResults.response = response;
+            // myResults.d = d;
+
+            allResults.push(myResults);
+
         });
 
         return_obj.responses_array = responses_array;
         return_obj.allResults = allResults;
         return_obj.full = d;
 
-        return return_obj;
+        return allResults;
     };
 
 
