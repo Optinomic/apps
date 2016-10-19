dc4f39036f5448c45623edde2b35733c4afde84d
Beat Ottiger <ottigerb@gmail.com>
2016-10-19 22:22:43 +0200

    changeDimensions

--- ch.suedhang.user.apps.tmt/main.js
+++ ch.suedhang.user.apps.tmt/main.js
@@ -94,6 +94,7 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
                 "variables": data.calculation_result.definitions.variables_array
             };
 
+            $scope.changeDimensions();
             $scope.d.loaded = true;
             console.log('(DATA): getCalculation | tmt_klinikstichprobe: ', $scope.d.calculations);
             console.log('(DATA): Data-Explorer: ', $scope.d.ks.result_explorer);
@@ -105,6 +106,50 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
         });
     };
 
+
+    $scope.changeDimensions = function() {
+
+        var current_ks = $scope.d.ks.result_explorer.ks;
+
+        var data_dive = current_ks.data;
+        var current_location = [];
+        var current_location_text = "";
+        var current_location_full = "";
+        var current_location_n_text = "";
+        var current_location_n = 0;
+
+        current_ks.dimensions.forEach(function(current_dim, myDimID) {
+            current_location.push(current_dim.selected.id);
+            if (current_location_text !== "") {
+                current_location_text = current_location_text + ' | '
+            };
+            current_location_text = current_location_text + current_dim.name + ': ' + current_dim.selected.text
+            data_dive = data_dive[current_dim.selected.id];
+        });
+
+
+        var my_data = null;
+        if (data_dive !== null) {
+            my_data = angular.copy(data_dive);
+            current_location_n = my_data.patients.length;
+            current_location_n_text = 'N=' + current_location_n;
+            current_location_full = current_location_text + ' (' + current_location_n_text + ')';
+        };
+
+        var location = {
+            "data": my_data,
+            "path": current_location,
+            "text": current_location_text,
+            "n_text": current_location_n_text,
+            "text_full": current_location_full,
+            "n": current_location_n
+        };
+
+        current_ks.location = location;
+
+
+        console.log('(Data) changeDimensions:', $scope.d.ks.result_explorer);
+    };
 
     // -------------------
     // TMT Init
