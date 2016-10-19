7aa64ac57bee9fb766952eb5679320d31617220c
Beat Ottiger <ottigerb@gmail.com>
2016-10-19 21:48:13 +0200

    result_explorer

--- ch.suedhang.user.apps.tmt/main.js
+++ ch.suedhang.user.apps.tmt/main.js
@@ -52,10 +52,51 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
         call.success(function(data) {
             // Save Data to $scope.d
             $scope.d.calculations = data.calculation_result;
+
+
+            $scope.d.ks.result_explorer = {
+                "types": {
+                    "all": [{
+                        "id": 0,
+                        "name": 'Alle Variablen'
+                    }, {
+                        "id": 1,
+                        "name": 'Einzelne Variablen'
+                    }],
+                    "selected": null,
+                    "selected_var": null
+                },
+                "ks": {}
+            };
+            $scope.d.ks.result_explorer.types.selected = ks.result_explorer.types.all[1];
+
+
+            // Set 'last' as Default
+            var dimensions = angular.copy(data.calculation_result.definitions.dimensions_app);
+            dimensions.forEach(function(current_dim, dimID) {
+                current_dim.selected = current_dim.array[current_dim.array.length - 1];
+            });
+
+            // Fokus Variable
+            $scope.d.ks.result_explorer.types.selected_var = data.calculation_result.definitions.variables_array[0];
+
+            $scope.d.ks.result_explorer.ks = {
+                "id": 0,
+                "data": data.calculation_result.md_patient_scores,
+                "date": new Date(),
+                "dimensions": data.calculation_result.definitions.dimensions_app,
+                "location": {},
+                "n_scores": data.calculation_result.patient_scores.length,
+                "variables": data.calculation_result.definitions.variables_array
+            };
+
             console.log('(DATA): getCalculation | tmt_klinikstichprobe: ', $scope.d.calculations);
+            console.log('(DATA): Data-Explorer: ', $scope.d.ks.result_explorer);
+
         });
-        call.error(function(data) {
-            console.log('(ERROR): getCalculation | tmt_klinikstichprobe:', data);
+
+        call.error(function(error) {
+            console.log('(ERROR): getCalculation | tmt_klinikstichprobe:', error);
         });
     };
 
