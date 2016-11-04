e6173fedb7ac8585043bee9a2d7b056df0ae735c
Beat Ottiger <ottigerb@gmail.com>
2016-11-04 13:33:49 +0100

    date

--- ch.suedhang.apps.tmt_V3/main.js
+++ ch.suedhang.apps.tmt_V3/main.js
@@ -194,7 +194,7 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
         var alle_messungen = angular.copy($scope.d.dataMain.calculations[0].calculation_results);
         console.log('(!) 0 - Messung', alle_messungen);
 
-        alle_messungen = dataService.sortByKey(alle_messungen, 'TMTBTime', 'desc');
+        alle_messungen = dataService.sortByKey(alle_messungen, 'date', 'desc');
         console.log('(!) 1 - Messung', alle_messungen);
 
 
