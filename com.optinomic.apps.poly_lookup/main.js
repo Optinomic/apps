d8c5468b31e93273aab0334ed1e3777e5226e87e
Beat Ottiger <ottigerb@gmail.com>
2016-11-17 17:00:24 +0100

    wip

--- com.optinomic.apps.poly_lookup/main.js
+++ com.optinomic.apps.poly_lookup/main.js
@@ -27,17 +27,11 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
 
             // Save Data to $scope.d
             $scope.d.dataMain = data;
-
-            // Check if we have survey_responses @ data.
-            if (data.survey_responses.length !== 0) {
-                console.log('(DATA): survey_responses:', data.survey_responses.length, data.survey_responses);
-                $scope.d.haveData = true;
+            $scope.d.haveData = true;
 
 
-                // Run App-Functions:
-                $scope.runODBC();
-
-            };
+            // Run App-Functions:
+            $scope.runODBC();
 
 
             // Finishing: Console Info & Init = done.
