7c72c16a39071434fe06a78b4dccc427cc4dda66
Beat Ottiger <ottigerb@gmail.com>
2016-11-24 18:23:58 +0100

    fix

--- ch.suedhang.apps.bdi/main.js
+++ ch.suedhang.apps.bdi/main.js
@@ -567,9 +567,9 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
     $scope.setPrintText = function() {
         $scope.d.text = {};
 
-        $scope.d.text.patient_anrede = $scope.d.dataMain.patient.data.extras.anrede;
-        $scope.d.text.messungen = $scope.d.dataMain.calculations["0"].calculation_results;
+        var patient_anrede = $scope.d.dataMain.patient.data.extras.anrede;
 
+        $scope.d.text.messungen = $scope.d.dataMain.calculations["0"].calculation_results;
         $scope.d.text.messungen = dataService.sortOn($scope.d.text.messungen, 'response.data.filled', true, false);
 
         $scope.d.text.text_start = patient_anrede + " wurde " + messungen.length + "x während des Aufenthaltes auf die Schwere der Depression anhand des Selbstbeurteilungsinstruments «BDI-II» getestet: ";
