7eee849884975d7c9fbb3e2cabed0f1b7f18224e
Beat Ottiger <ottigerb@gmail.com>
2016-12-03 16:58:35 +0100

    (DEBUG) messung_obj

--- ch.suedhang.apps.isk/main.js
+++ ch.suedhang.apps.isk/main.js
@@ -92,7 +92,7 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
         console.log('(✓) Klinikstichprobe geladen: ', $scope.d.ks);
 
         // Follow the white rabbit
-        // $scope.initTMT();
+        $scope.initISK();
     };
 
 
@@ -145,14 +145,14 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
     };
 
 
-    $scope.initTMT = function() {
+    $scope.initISK = function() {
 
-        $scope.d.TMT = {};
-        $scope.d.TMT.init = false;
-        $scope.d.TMT.show_legend = false;
+        $scope.d.ISK = {};
+        $scope.d.ISK.init = false;
+        $scope.d.ISK.show_legend = false;
 
         // Default Z-Score Option
-        $scope.d.TMT.zscore_options = {
+        $scope.d.ISK.zscore_options = {
             "zscore_min": -6,
             "zscore_max": 6,
             "clinicsample_color": "#C5CAE9",
@@ -165,7 +165,7 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
 
 
         // Toggles | Grafiken
-        $scope.d.TMT.toggles = {
+        $scope.d.ISK.toggles = {
             "show_text": true,
             "show_clinicsample": true,
             "show_clinicsample_scores": false
@@ -173,11 +173,11 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
 
 
         // Gruppierung der Messungen
-        $scope.d.TMT.groups = [{
-            "name": "TMT A",
+        $scope.d.ISK.groups = [{
+            "name": "ISK A",
             "data": []
         }, {
-            "name": "TMT B",
+            "name": "ISK B",
             "data": []
         }];
 
@@ -189,143 +189,139 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
         dataService.sortOn(alle_messungen, 'date', false, false);
 
 
-        // Loop alle_messungen und messung in TMT A / TMT B pushen
+        // Loop alle_messungen und messung in ISK A / ISK B pushen
         alle_messungen.forEach(function(messung, messungID) {
 
             // console.log('(!) 1 - Messung', messungID, messung);
 
 
             // Variablen vorbereiten | verdrahten.
-            var mz_id = messung.Messzeitpunkt.Messzeitpunkt - 1;
-            var mz_text = messung.Messzeitpunkt.Messzeitpunkt_Text;
-            var datum_messung = $filter('date')(messung.date);
-            var zeit_messung = messung.date.substring(11, 16);
-
-            var zscore_A = messung.percentile.z_scores.tmtA_z_rounded;
-            var zscore_B = messung.percentile.z_scores.tmtB_z_rounded;
-
-            var zeitabbruch_A = messung.percentile.z_scores.tmtA_z_zeitabbruch_rounded;
-            var zeitabbruch_B = messung.percentile.z_scores.tmtB_z_zeitabbruch_rounded;
+            var mz_id = messung.info.mz.mz_id;
+            var mz_text = messung.info.mz.mz_text;
+            var datum_messung = $filter('date')(messung.info.response.Datum);
+            var zeit_messung = messung.info.response.Datum.substring(11, 16);
 
             // Eintritt / Austritt / Anderer MZ
             var cs_color = ['#9E9E9E', '#EEEEEE', '#ebebeb'];
             var current_cs_color = cs_color[mz_id];
 
-
-            // $scope.d.TMT.zscore_options anpassen
-            if (Math.abs(zscore_A) > (Math.abs($scope.d.TMT.zscore_options.zscore_min) - 0.5)) {
-                $scope.d.TMT.zscore_options.zscore_min = (Math.abs(zscore_A) + 1) * -1;
-            };
-
-            if (Math.abs(zscore_B) > (Math.abs($scope.d.TMT.zscore_options.zscore_max) - 0.5)) {
-                $scope.d.TMT.zscore_options.zscore_max = (Math.abs(zscore_B) + 1);
-            };
-
-
-
-            // Default Pfad für MD-Array erstellen
             var dimensions_path = [];
-            var current_ks = $scope.d.ks;
-            current_ks.dimensions.forEach(function(current_dim, myDimID) {
 
-                var default_last = current_dim.array.length - 1;
-                dimensions_path[myDimID] = default_last;
+            //    var zscore_A = messung.percentile.z_scores.tmtA_z_rounded;
+            //    var zscore_B = messung.percentile.z_scores.tmtB_z_rounded;
+            //    
+            //    var zeitabbruch_A = messung.percentile.z_scores.tmtA_z_zeitabbruch_rounded;
+            //    var zeitabbruch_B = messung.percentile.z_scores.tmtB_z_zeitabbruch_rounded;
+            //    
 
-                if (current_dim.name === "Altersgruppe") {
-                    dimensions_path[myDimID] = messung.percentile.age_perz.altersgruppe;
-                };
+            //    
+            //    
+            //    // $scope.d.ISK.zscore_options anpassen
+            //    if (Math.abs(zscore_A) > (Math.abs($scope.d.ISK.zscore_options.zscore_min) - 0.5)) {
+            //        $scope.d.ISK.zscore_options.zscore_min = (Math.abs(zscore_A) + 1) * -1;
+            //    };
+            //    
+            //    if (Math.abs(zscore_B) > (Math.abs($scope.d.ISK.zscore_options.zscore_max) - 0.5)) {
+            //        $scope.d.ISK.zscore_options.zscore_max = (Math.abs(zscore_B) + 1);
+            //    };
+            //    
+            //    
+            //    
+            //    // Default Pfad für MD-Array erstellen
+            //    
+            //    var current_ks = $scope.d.ks;
+            //    current_ks.dimensions.forEach(function(current_dim, myDimID) {
+            //    
+            //        var default_last = current_dim.array.length - 1;
+            //        dimensions_path[myDimID] = default_last;
+            //    
+            //    
+            //        if (current_dim.name === "Messzeitpunkt") {
+            //            dimensions_path[myDimID] = mz_id;
+            //        };
+            //    });
+            //    // console.log('(!) 2 - dimensions_path', dimensions_path);
+            //    
+            //    
+            //    // var md_data = $scope.getKSLocation(dimensions_path);
+            //    // console.log('(!) 3 - md_data', dimensions_path, md_data);
+            //    
+            //    
+            //    // Resultate in Gruppen schreiben
+            //    $scope.d.ISK.groups.forEach(function(group, groupID) {
 
-                if (current_dim.name === "Ausbildungsgrad") {
-                    if (messung.edu_years > 12) {
-                        dimensions_path[myDimID] = 1;
-                    } else {
-                        dimensions_path[myDimID] = 0;
-                    };
-                };
+            var messung_obj = {
+                "calculation": messung,
+                "ks": {
+                    //"path_data": md_data,
+                    "path_data": null,
+                    "path_selected": dimensions_path,
+                    "show_controls": false
+                },
+                "zscore": {
+                    "zscore": null,
+                    "zscore_color": '#1A237E',
+                    "text_left": mz_text,
+                    "text_left_caption": "ISK",
+                    "text_right": datum_messung,
+                    "text_right_caption": "",
+                    "clinicsample_start": 0,
+                    "clinicsample_end": 0,
+                    "clinicsample_color": current_cs_color,
+                    "marker_1_score": null,
+                    "marker_1_text": "",
+                    "marker_1_color": "#F44336",
+                },
+            };
 
-                if (current_dim.name === "Messzeitpunkt") {
-                    dimensions_path[myDimID] = mz_id;
-                };
-            });
-            // console.log('(!) 2 - dimensions_path', dimensions_path);
+            console.log('(DEBUG) messung_obj', messung_obj);
 
-
-            // var md_data = $scope.getKSLocation(dimensions_path);
-            // console.log('(!) 3 - md_data', dimensions_path, md_data);
-
-
-            // Resultate in Gruppen schreiben
-            $scope.d.TMT.groups.forEach(function(group, groupID) {
-
-                var messung_obj = {
-                    "calculation": messung,
-                    "ks": {
-                        //"path_data": md_data,
-                        "path_data": null,
-                        "path_selected": dimensions_path,
-                        "show_controls": false
-                    },
-                    "zscore": {
-                        "zscore": null,
-                        "zscore_color": '#1A237E',
-                        "text_left": mz_text,
-                        "text_left_caption": "TMT",
-                        "text_right": datum_messung,
-                        "text_right_caption": "",
-                        "clinicsample_start": 0,
-                        "clinicsample_end": 0,
-                        "clinicsample_color": current_cs_color,
-                        "marker_1_score": null,
-                        "marker_1_text": "Zeitabbruch",
-                        "marker_1_color": "#F44336",
-                    },
-                };
-
-                if (group.name === 'TMT A') {
-                    messung_obj.zscore.text_left_caption = group.name;
-                    messung_obj.zscore.zscore = zscore_A;
-                    messung_obj.zscore.marker_1_score = zeitabbruch_A;
-                    // messung_obj.zscore.clinicsample_start = $scope.roundToTwo(md_data.statistics['TMTAZ'].mean_1sd_min);
-                    // messung_obj.zscore.clinicsample_end = $scope.roundToTwo(md_data.statistics['TMTAZ'].mean_1sd_plus);
-
-                    group.data.push(messung_obj);
-                };
-
-                if (group.name === 'TMT B') {
-                    messung_obj.zscore.text_left_caption = group.name;
-                    messung_obj.zscore.zscore = zscore_B;
-                    messung_obj.zscore.marker_1_score = zeitabbruch_B;
-                    // messung_obj.zscore.clinicsample_start = $scope.roundToTwo(md_data.statistics['TMTBZ'].mean_1sd_min);
-                    // messung_obj.zscore.clinicsample_end = $scope.roundToTwo(md_data.statistics['TMTBZ'].mean_1sd_plus);
-
-                    group.data.push(messung_obj);
-                };
-
-
-                // Auffällige Testleistung |  färben
-                if (messung_obj.zscore.zscore < messung_obj.zscore.clinicsample_start) {
-                    // Auffällige Testleistung: Rot
-                    messung_obj.zscore.zscore_color = '#C62828';
-                };
-                if (messung_obj.zscore.zscore > messung_obj.zscore.clinicsample_end) {
-                    // Auffällige Testleistung: Grün
-                    messung_obj.zscore.zscore_color = '#2E7D32';
-                };
-            });
+            //    if (group.name === 'ISK A') {
+            //        messung_obj.zscore.text_left_caption = group.name;
+            //        messung_obj.zscore.zscore = zscore_A;
+            //        messung_obj.zscore.marker_1_score = zeitabbruch_A;
+            //        // messung_obj.zscore.clinicsample_start = $scope.roundToTwo(md_data.statistics['ISKAZ'].mean_1sd_min);
+            //        // messung_obj.zscore.clinicsample_end = $scope.roundToTwo(md_data.statistics['ISKAZ'].mean_1sd_plus);
+            //    
+            //        group.data.push(messung_obj);
+            //    };
+            //    
+            //    if (group.name === 'ISK B') {
+            //        messung_obj.zscore.text_left_caption = group.name;
+            //        messung_obj.zscore.zscore = zscore_B;
+            //        messung_obj.zscore.marker_1_score = zeitabbruch_B;
+            //        // messung_obj.zscore.clinicsample_start = $scope.roundToTwo(md_data.statistics['ISKBZ'].mean_1sd_min);
+            //        // messung_obj.zscore.clinicsample_end = $scope.roundToTwo(md_data.statistics['ISKBZ'].mean_1sd_plus);
+            //    
+            //        group.data.push(messung_obj);
+            //    };
+            //    
+            //    
+            //    // Auffällige Testleistung |  färben
+            //    if (messung_obj.zscore.zscore < messung_obj.zscore.clinicsample_start) {
+            //        // Auffällige Testleistung: Rot
+            //        messung_obj.zscore.zscore_color = '#C62828';
+            //    };
+            //    if (messung_obj.zscore.zscore > messung_obj.zscore.clinicsample_end) {
+            //        // Auffällige Testleistung: Grün
+            //        messung_obj.zscore.zscore_color = '#2E7D32';
+            //    };
+            //        
+            //    });
 
         });
 
         // MD - Daten befüllen
-        $scope.d.TMT.groups.forEach(function(group, groupID) {
+        $scope.d.ISK.groups.forEach(function(group, groupID) {
             group.data.forEach(function(groupInner, groupInnerID) {
-                $scope.changeClinicSample(groupInner);
-                //console.log('(!) 3 - changeClinicSample', groupID, groupInnerID, groupInner);
+                // $scope.changeClinicSample(groupInner);
+                console.log('(!) 3 - changeClinicSample', groupID, groupInnerID, groupInner);
             });
         });
-        $scope.d.TMT.show_legend = false;
 
+        $scope.d.ISK.show_legend = false;
 
-        $scope.d.TMT.init = true;
+        $scope.d.ISK.init = true;
     };
 
 
@@ -334,14 +330,14 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
         current_sample.ks.path_data = $scope.getKSLocation(current_sample.ks.path_selected);
 
         if (current_sample.ks.path_data.statistics !== null) {
-            if (current_sample.zscore.text_left_caption === 'TMT A') {
-                current_sample.zscore.clinicsample_start = $scope.roundToTwo(current_sample.ks.path_data.statistics['TMTAZ'].mean_1sd_min);
-                current_sample.zscore.clinicsample_end = $scope.roundToTwo(current_sample.ks.path_data.statistics['TMTAZ'].mean_1sd_plus);
+            if (current_sample.zscore.text_left_caption === 'ISK A') {
+                current_sample.zscore.clinicsample_start = $scope.roundToTwo(current_sample.ks.path_data.statistics['ISKAZ'].mean_1sd_min);
+                current_sample.zscore.clinicsample_end = $scope.roundToTwo(current_sample.ks.path_data.statistics['ISKAZ'].mean_1sd_plus);
             };
 
-            if (current_sample.zscore.text_left_caption === 'TMT B') {
-                current_sample.zscore.clinicsample_start = $scope.roundToTwo(current_sample.ks.path_data.statistics['TMTBZ'].mean_1sd_min);
-                current_sample.zscore.clinicsample_end = $scope.roundToTwo(current_sample.ks.path_data.statistics['TMTBZ'].mean_1sd_plus);
+            if (current_sample.zscore.text_left_caption === 'ISK B') {
+                current_sample.zscore.clinicsample_start = $scope.roundToTwo(current_sample.ks.path_data.statistics['ISKBZ'].mean_1sd_min);
+                current_sample.zscore.clinicsample_end = $scope.roundToTwo(current_sample.ks.path_data.statistics['ISKBZ'].mean_1sd_plus);
             };
 
 
@@ -363,7 +359,7 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
             current_sample.zscore.clinicsample_end = 0;
         };
 
-        $scope.d.TMT.show_legend = true;
+        $scope.d.ISK.show_legend = true;
 
         console.log('(Done) changeClinicSample', current_sample);
     };
