3a520202b774c4254b8a11a1fdc87db5793feb8d
Beat Ottiger <ottigerb@gmail.com>
2016-12-06 16:46:09 +0100

    wip

--- com.optinomic.user.apps.poly_stay/task.js
+++ com.optinomic.user.apps.poly_stay/task.js
@@ -1,5 +1,100 @@
 function main(token) {
+
+
+    function Promise(fn) {
+        var state = 'pending';
+        var value;
+        var deferred = null;
+
+        function resolve(newValue) {
+            try {
+                if (newValue && typeof newValue.then === 'function') {
+                    newValue.then(resolve, reject);
+                    return;
+                }
+                state = 'resolved';
+                value = newValue;
+
+                if (deferred) {
+                    handle(deferred);
+                }
+            } catch (e) {
+                reject(e);
+            }
+        }
+
+        function reject(reason) {
+            state = 'rejected';
+            value = reason;
+
+            if (deferred) {
+                handle(deferred);
+            }
+        }
+
+        function handle(handler) {
+            if (state === 'pending') {
+                deferred = handler;
+                return;
+            }
+
+            var handlerCallback;
+
+            if (state === 'resolved') {
+                handlerCallback = handler.onResolved;
+            } else {
+                handlerCallback = handler.onRejected;
+            }
+
+            if (!handlerCallback) {
+                if (state === 'resolved') {
+                    handler.resolve(value);
+                } else {
+                    handler.reject(value);
+                }
+
+                return;
+            }
+
+            var ret;
+            try {
+                ret = handlerCallback(value);
+                handler.resolve(ret);
+            } catch (e) {
+                handler.reject(e);
+            }
+        }
+
+        this.then = function(onResolved, onRejected) {
+            return new Promise(function(resolve, reject) {
+                handle({
+                    onResolved: onResolved,
+                    onRejected: onRejected,
+                    resolve: resolve,
+                    reject: reject
+                });
+            });
+        };
+
+        fn(resolve, reject);
+    }
+
+
+
     helpers.callAPI("GET", "/patients", null, null, function(resp) {
-        console.log(resp.responseText);
+
+
+        var patients = resp.responseText.patients;
+        console.log('(!)', patients);
+
+
+        for (var pID = 0; pID < patients.length; pID++) {
+
+            var current_patient = patients[pID];
+            console.log('(+)', pID, current_patient);
+
+        };
+
+
     });
 }
