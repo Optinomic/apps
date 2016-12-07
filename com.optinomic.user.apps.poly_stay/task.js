fa08b31bae24ef9faa11b961155a7e0b4e9f29f3
Beat Ottiger <ottigerb@gmail.com>
2016-12-07 19:48:13 +0100

    wip

--- com.optinomic.user.apps.poly_stay/task.js
+++ com.optinomic.user.apps.poly_stay/task.js
@@ -5,213 +5,306 @@ function main(token) {
     //    therapy-server-components task-runner /var/therapyserver/config/config.json com.optinomic.apps.poly_stay get_belegung
 
 
-    function Promise(fn) {
-        var state = 'pending';
-        var value;
-        var deferred = null;
+    // Currently on STAY.
+    var patient_filters = {
+        in_stay: "True"
+    };
 
-        function resolve(newValue) {
-            try {
-                if (newValue && typeof newValue.then === 'function') {
-                    newValue.then(resolve, reject);
-                    return;
-                }
-                state = 'resolved';
-                value = newValue;
 
-                if (deferred) {
-                    handle(deferred);
-                }
-            } catch (e) {
-                reject(e);
-            }
+    var log = {
+        "date": new Date(),
+        "count": {
+            "patients": 0,
+            "stays": 0,
+            "bel_odbc": 0,
+            "annotiations": 0
+        },
+        "done": {
+            "patients": [],
+            "stays": [],
+            "bel_odbc": [],
+            "annotiations": []
         }
+    }
+};
 
-        function reject(reason) {
-            state = 'rejected';
-            value = reason;
+
+
+function Promise(fn) {
+    var state = 'pending';
+    var value;
+    var deferred = null;
+
+    function resolve(newValue) {
+        try {
+            if (newValue && typeof newValue.then === 'function') {
+                newValue.then(resolve, reject);
+                return;
+            }
+            state = 'resolved';
+            value = newValue;
 
             if (deferred) {
                 handle(deferred);
             }
+        } catch (e) {
+            reject(e);
+        }
+    }
+
+    function reject(reason) {
+        state = 'rejected';
+        value = reason;
+
+        if (deferred) {
+            handle(deferred);
+        }
+    }
+
+    function handle(handler) {
+        if (state === 'pending') {
+            deferred = handler;
+            return;
         }
 
-        function handle(handler) {
-            if (state === 'pending') {
-                deferred = handler;
-                return;
-            }
+        var handlerCallback;
 
-            var handlerCallback;
+        if (state === 'resolved') {
+            handlerCallback = handler.onResolved;
+        } else {
+            handlerCallback = handler.onRejected;
+        }
 
+        if (!handlerCallback) {
             if (state === 'resolved') {
-                handlerCallback = handler.onResolved;
+                handler.resolve(value);
             } else {
-                handlerCallback = handler.onRejected;
+                handler.reject(value);
             }
 
-            if (!handlerCallback) {
-                if (state === 'resolved') {
-                    handler.resolve(value);
-                } else {
-                    handler.reject(value);
-                }
-
-                return;
-            }
-
-            var ret;
-            try {
-                ret = handlerCallback(value);
-                handler.resolve(ret);
-            } catch (e) {
-                handler.reject(e);
-            }
+            return;
         }
 
-        this.then = function(onResolved, onRejected) {
-            return new Promise(function(resolve, reject) {
-                handle({
-                    onResolved: onResolved,
-                    onRejected: onRejected,
-                    resolve: resolve,
-                    reject: reject
-                });
-            });
-        };
+        var ret;
+        try {
+            ret = handlerCallback(value);
+            handler.resolve(ret);
+        } catch (e) {
+            handler.reject(e);
+        }
+    }
 
-        fn(resolve, reject);
-    };
-
-
-    function getStays(patient_id) {
-        // GET /patients/:patient_id/stays
+    this.then = function(onResolved, onRejected) {
         return new Promise(function(resolve, reject) {
-
-            var api_call = "/patients/" + patient_id + "/stays";
-            console.log('(?) api_call, ', api_call);
-
-            helpers.callAPI("GET", api_call, null, null, function(resp_stay) {
-                var stay_response = JSON.parse(resp_stay.responseText);
-                var stays = stay_response.stays;
-
-
-                for (var sID = 0; sID < stays.length; sID++) {
-                    var stay = stays[sID];
-
-                    var cis_fid_str = stay.data.cis_fid.toString();
-                    cis_fid_str = cis_fid_str.substring(0, (cis_fid_str.length - 2));
-
-                    stay.poly_pid = parseInt(cis_fid_str.substring(0, (cis_fid_str.length - 2)));
-                    stay.poly_fid = parseInt(cis_fid_str.substring((cis_fid_str.length - 2), (cis_fid_str.length)));
-
-                    var sql = include_as_js_string(belegung_history_from_fid.sql);
-                    sql = sql.replace("%poly_pid%", stay.poly_pid);
-                    sql = sql.replace("%poly_fid%", stay.poly_fid);
-
-                    stay.sql = sql;
-
-                    console.log('(!) stay =', patient_id, stays.length, cis_fid_str, stay.poly_pid, stay.poly_fid);
-                };
-
-
-
-                resolve(JSON.stringify(stays));
+            handle({
+                onResolved: onResolved,
+                onRejected: onRejected,
+                resolve: resolve,
+                reject: reject
             });
         });
     };
 
-
-    function getODBCBelegung(my_stay) {
-
-        console.log('---getODBCBelegung', my_stay);
-        // GET /patients/:patient_id/stays
-        return new Promise(function(resolve, reject) {
+    fn(resolve, reject);
+};
 
 
-            var body = {
-                "query": my_stay.sql,
-                "delimiter": ";",
-                "direct": "True",
-                "format": "json"
+function getStays(patient_id) {
+    // GET /patients/:patient_id/stays
+    return new Promise(function(resolve, reject) {
+
+        var api_call = "/patients/" + patient_id + "/stays";
+        console.log('(?) api_call, ', api_call);
+
+        helpers.callAPI("GET", api_call, null, null, function(resp_stay) {
+            var stay_response = JSON.parse(resp_stay.responseText);
+            var stays = stay_response.stays;
+
+
+            for (var sID = 0; sID < stays.length; sID++) {
+                var stay = stays[sID];
+
+                var cis_fid_str = stay.data.cis_fid.toString();
+                cis_fid_str = cis_fid_str.substring(0, (cis_fid_str.length - 2));
+
+                stay.poly_pid = parseInt(cis_fid_str.substring(0, (cis_fid_str.length - 2)));
+                stay.poly_fid = parseInt(cis_fid_str.substring((cis_fid_str.length - 2), (cis_fid_str.length)));
+
+                var sql = include_as_js_string(belegung_history_from_fid.sql);
+                sql = sql.replace("%poly_pid%", stay.poly_pid);
+                sql = sql.replace("%poly_fid%", stay.poly_fid);
+
+                stay.sql = sql;
+
+                console.log('(!) stay =', patient_id, stays.length, cis_fid_str, stay.poly_pid, stay.poly_fid);
             };
 
 
-            var api_call = "/data_sources/Polypoint/query";
 
-
-            my_stay.belegung = {
-                "body": body,
-                "api_str": api_call
-            };
-
-
-            //   helpers.callAPI("GET", api_call, null, null, function(resp_stay) {
-            //   
-            //   
-            //       
-            //   });
-
-
-            resolve(JSON.stringify(my_stay));
-
+            resolve(JSON.stringify(stays));
         });
-    };
+    });
+};
 
 
+function getODBCBelegung(my_stay) {
 
-    // Currently on STAY.
-
-
-    var filters = {
-        in_stay: "True"
-    };
-
-    helpers.callAPI("GET", "/patients", filters, null, function(resp) {
-
-        var response = JSON.parse(resp.responseText);
-        var patients = response.patients;
+    console.log('---getODBCBelegung', my_stay);
+    // GET /patients/:patient_id/stays
+    return new Promise(function(resolve, reject) {
 
 
-        for (var pID = 0; pID < patients.length; pID++) {
+        var polypoint_belegung = {
+            "query": my_stay.sql,
+            "delimiter": ";",
+            "direct": "True",
+            "format": "json"
+        };
 
-            var current_patient = patients[pID];
-            var patient_id = parseInt(current_patient.id);
-
-            // console.log('(+)', pID, patient_id, current_patient.data.last_name);
+        var api_call = "/data_sources/Polypoint/query";
 
 
-            getStays(current_patient.id).then(function(stay_json) {
-                var stays = JSON.parse(stay_json);
+        var belegung = {
+            "art": [{
+                "bel_id": 0,
+                "name": "Unbekannt",
+                "description": "Unbekannt / Nicht festgelegt"
+            }, {
+                "bel_id": 1,
+                "name": "EAS",
+                "description": "Entzugs- und Abklärungsstation"
+            }, {
+                "bel_id": 2,
+                "name": "EP",
+                "description": "Entwöhnungsprogramm"
+            }, {
+                "bel_id": 3,
+                "name": "EAS & EP",
+                "description": "Entzugs- & Abklärungsstation sowie Entwöhnungsprogramm"
+            }, {
+                "bel_id": 4,
+                "name": "TK",
+                "description": "Tagesklinik"
+            }],
+            "current": {}
+        };
+        belegung.current = belegung.art[0];
 
-                for (var sID = 0; sID < stays.length; sID++) {
 
-                    var current_stay = stays[sID];
-                    var stay_id = parseInt(current_stay.id);
+        var belegung = {
+            "body": body,
+            "api_str": api_call
+        };
 
-                    // console.log('---current_stay', current_stay);
-
-                    getODBCBelegung(current_stay).then(function(bel_json) {
-                        var bel = JSON.parse(bel_json);
-                        console.log('(✓) BEL-DATA, ', patients.length, current_patient.id, stay_id, bel);
-
-                    }).then(null, function(error) {
-                        console.log('(!) BEL-ERROR, ', error);
-                    });
-
-                };
-
-                // console.log('(✓) STAY-DATA, ', patients.length, current_stay);
-
-            }).then(null, function(error) {
-                console.log('(!) ERROR, ', error);
-            });
-
+        var annotation_obj = {
+            "bel_selector": stay.belegung.current,
+            "bel_all": polypoint_belegung
         };
 
 
-        // console.log('(!) Total Patients =', patients.length);
+        //   helpers.callAPI("GET", api_call, null, null, function(resp_stay) {
+        //   
+        //   
+        //       
+        //   });
 
+
+        resolve(JSON.stringify(annotation_obj));
 
     });
+};
+
+
+function writeBelegung(my_stay) {
+
+    console.log('---getODBCBelegung', my_stay);
+    // GET /patients/:patient_id/stays
+    return new Promise(function(resolve, reject) {
+
+
+        var body = {
+            "query": my_stay.sql,
+            "delimiter": ";",
+            "direct": "True",
+            "format": "json"
+        };
+
+
+        var api_call = "/data_sources/Polypoint/query";
+
+
+        my_stay.belegung = {
+            "body": body,
+            "api_str": api_call
+        };
+
+
+        //   helpers.callAPI("GET", api_call, null, null, function(resp_stay) {
+        //   
+        //   
+        //       
+        //   });
+
+
+        resolve(JSON.stringify(my_stay));
+
+    });
+};
+
+
+
+
+
+helpers.callAPI("GET", "/patients", patient_filters, null, function(resp) {
+
+    var response = JSON.parse(resp.responseText);
+    var patients = response.patients;
+    log.count.patients = patients.length;
+
+
+    for (var pID = 0; pID < patients.length; pID++) {
+
+        var current_patient = patients[pID];
+        var patient_id = parseInt(current_patient.id);
+        log.done.patients.push(patient_id);
+
+
+        // console.log('(+)', pID, patient_id, current_patient.data.last_name);
+
+        getStays(current_patient.id).then(function(stay_json) {
+            var stays = JSON.parse(stay_json);
+            log.count.stays = log.count.stays + stays.length;
+
+            for (var sID = 0; sID < stays.length; sID++) {
+
+                var current_stay = stays[sID];
+                var stay_id = parseInt(current_stay.id);
+                log.done.stays.push(stay_id);
+
+
+                // console.log('---current_stay', current_stay);
+
+                getODBCBelegung(current_stay).then(function(bel_json) {
+                    var bel = JSON.parse(bel_json);
+                    console.log('(✓) BEL-DATA, ', patients.length, bel, log);
+
+                }).then(null, function(error) {
+                    console.log('(!) BEL-ERROR, ', error);
+                });
+
+            };
+
+            // console.log('(✓) STAY-DATA, ', patients.length, current_stay);
+
+        }).then(null, function(error) {
+            console.log('(!) ERROR, ', error);
+        });
+
+    };
+
+
+    // console.log('(!) Total Patients =', patients.length);
+
+
+});
 }
