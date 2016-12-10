b46819f752de0ea0d68b23f307c94a910dc2b68f
Beat Ottiger <ottigerb@gmail.com>
2016-12-10 12:10:41 +0100

    wip

--- com.optinomic.user.apps.poly_stay/task.js
+++ com.optinomic.user.apps.poly_stay/task.js
@@ -303,10 +303,10 @@ function main(token) {
                     "value": JSON.stringify(annot_obj)
                 };
 
-                // console.log('writeBelegung:', patient_id, body.value);
+                console.log('writeBelegung:', patient_id, body.value);
 
                 helpers.callAPI("PUT", apiStr, null, body, function(resp_write) {
-                    console.log(' -> write ', patient_id, JSON.parse(resp_write.responseText));
+                    console.log(' -> write ', patient_id);
                     resolve(JSON.stringify(annot_obj));
                 });
 
