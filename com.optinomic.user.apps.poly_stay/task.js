01481ff04dc6ca7d0855a23abfddb6049ad74e00
Beat Ottiger <ottigerb@gmail.com>
2016-12-10 10:15:31 +0100

    null

--- com.optinomic.user.apps.poly_stay/task.js
+++ com.optinomic.user.apps.poly_stay/task.js
@@ -225,7 +225,7 @@ function main(token) {
 
                 console.log('resp_bel', resp_bel);
 
-                if (resp_bel) {
+                if (resp_bel.responseText !== null) {
                     var bel_response = JSON.parse(resp_bel.responseText);
                 } else {
                     var bel_response = null;
