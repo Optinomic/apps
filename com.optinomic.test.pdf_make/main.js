8a631955056c338eb9ac020385a1a0f383686016
Beat Ottiger <ottigerb@gmail.com>
2016-12-12 13:23:41 +0100

    iframe 2

--- com.optinomic.test.pdf_make/main.js
+++ com.optinomic.test.pdf_make/main.js
@@ -45,8 +45,30 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
     // -----------------------------------
 
     $scope.pdf_make_init = function() {
-        $scope.d.docDefinition = { content: 'This is an sample PDF printed with pdfMake' };
+        $scope.d.docDefinition = {
+            content: [
+                // if you don't need styles, you can use a simple string to define a paragraph
+                'This is a standard paragraph, using default style',
 
+                // using a { text: '...' } object lets you set styling properties
+                { text: 'This paragraph will have a bigger font', fontSize: 15 },
+
+                // if you set the value of text to an array instead of a string, you'll be able
+                // to style any part individually
+                {
+                    text: [
+                        'This paragraph is defined as an array of elements to make it possible to ',
+                        { text: 'restyle part of it and make it bigger ', fontSize: 15 },
+                        'than the rest.'
+                    ]
+                }
+            ]
+        };
+
+        $scope.pdf_create();
+    };
+
+    $scope.pdf_create = function() {
         const pdfDocGenerator = pdfMake.createPdf($scope.d.docDefinition);
         pdfDocGenerator.getDataUrl((dataUrl) => {
             const targetElement = document.querySelector('#iframeContainer');
