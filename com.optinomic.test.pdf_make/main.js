e3a08c6c0424f49ba490c7ab905f6a5083bdcb6f
Beat Ottiger <ottigerb@gmail.com>
2016-12-12 13:54:49 +0100

    remove image

--- com.optinomic.test.pdf_make/main.js
+++ com.optinomic.test.pdf_make/main.js
@@ -50,22 +50,6 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
                 // if you don't need styles, you can use a simple string to define a paragraph
                 $scope.d.dataMain.config.data.customer.contact.name,
 
-                {
-                    image: 'http://www.optinomic.com/wp-content/uploads/2014/09/optinomic_logo_medium.png'
-                },
-
-                // using a { text: '...' } object lets you set styling properties
-                { text: 'This paragraph will have a bigger font', fontSize: 15 },
-
-                // if you set the value of text to an array instead of a string, you'll be able
-                // to style any part individually
-                {
-                    text: [
-                        'This paragraph is defined as an array of elements to make it possible to ',
-                        { text: 'restyle part of it and make it bigger ', fontSize: 15 },
-                        'than the rest.'
-                    ]
-                }
             ]
         };
 
