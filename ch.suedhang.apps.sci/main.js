1a4c9f78e98ab48f30932eedf7e5bd231a94a71c
Beat Ottiger <ottigerb@gmail.com>
2016-12-16 10:42:45 +0100

    sci_stanine

--- ch.suedhang.apps.sci/main.js
+++ ch.suedhang.apps.sci/main.js
@@ -42,7 +42,7 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
                     $scope.setStanineView();
                 };
 
-                if (current_template === 'print_sci_report_only') {
+                if (current_template === 'sci_stanine') {
                     $scope.setStanineView();
                     $scope.groupStanineView();
                 };
