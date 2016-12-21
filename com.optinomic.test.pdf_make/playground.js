b496981c5e7b51f82d3583a026c8d178bfab7d7b
Beat Ottiger <ottigerb@gmail.com>
2016-12-21 17:39:22 +0100

    TMT | Fix Zeit / Fehler

--- com.optinomic.test.pdf_make/playground.js
+++ com.optinomic.test.pdf_make/playground.js
@@ -26,6 +26,26 @@ var dd = {
         return obj;
     },
     "content": [{
+        "text": "keepTogether",
+        "style": "h1"
+    }, {
+        "table": {
+            "dontBreakRows": true,
+            "headerRows": 0,
+            "body": [
+                [{
+                    "stack": [{
+                        "text": "1 keepTogether",
+                        "style": "h1"
+                    }, {
+                        "text": "p keepTogether",
+                        "style": "p"
+                    }]
+                }]
+            ]
+        },
+        layout: 'noBorders'
+    }, {
         "text": "Stanine",
         "style": "h2"
     }, {
@@ -246,221 +266,225 @@ var dd = {
             "style": "caption"
         }]
     }, {
-        "text": "Überschrift",
-        "style": "h2"
-    }, {
-        "text": "Dies ist ein Standardtext",
-        "style": "text"
-    }, {
-        "alignment": "left",
-        "columns": [{
-            "width": 110,
-            "fontSize": 10,
-            "alignment": "right",
-            "text": "LINKS: Dies ist ein Standardtext",
-            "margin": [0, 14, 0, 0]
+        "stack": [{
+            "text": "Überschrift",
+            "margin": [0, 6, 0, 0],
+            "style": "h3"
         }, {
-            "width": "auto",
-            "stack": [{
-                "columns": [
-                    { "text": "Eintritt", "alignment": "left" },
-                    { "text": "Datum", "alignment": "right" }
-                ],
-                "fontSize": 10,
-                "color": "#212121",
-                "margin": [0, 3, 0, 1]
-            }, {
-                canvas: [{
-                    type: "rect",
-                    x: 0,
-                    y: 0,
-                    w: 280,
-                    h: 30,
-                    "lineColor": "#E0E0E0"
-                }, {
-                    type: "rect",
-                    x: 80,
-                    y: 0,
-                    w: 80,
-                    h: 30,
-                    "lineColor": "#E0E0E0",
-                    "color": "#EEEEEE"
-                }, {
-                    type: "line",
-                    x1: 46.66,
-                    y1: 0,
-                    x2: 46.66,
-                    y2: 30,
-                    lineWidth: 1,
-                    "lineColor": "#E0E0E0"
-                }, {
-                    type: "line",
-                    x1: 93.33,
-                    y1: 0,
-                    x2: 93.33,
-                    y2: 30,
-                    lineWidth: 1,
-                    "lineColor": "#E0E0E0"
-                }, {
-                    type: "line",
-                    x1: 140,
-                    y1: 0,
-                    x2: 140,
-                    y2: 30,
-                    lineWidth: 1,
-                    "lineColor": "#424242"
-                }, {
-                    type: "line",
-                    x1: 0,
-                    y1: 25,
-                    x2: 0,
-                    y2: 35,
-                    lineWidth: 1,
-                    "lineColor": "#9E9E9E"
-                }, {
-                    type: "line",
-                    x1: 46.66,
-                    y1: 25,
-                    x2: 46.66,
-                    y2: 35,
-                    lineWidth: 1,
-                    "lineColor": "#9E9E9E"
-                }, {
-                    type: "line",
-                    x1: 93.33,
-                    y1: 25,
-                    x2: 93.33,
-                    y2: 35,
-                    lineWidth: 1,
-                    "lineColor": "#9E9E9E"
-                }, {
-                    type: "line",
-                    x1: 140,
-                    y1: 25,
-                    x2: 140,
-                    y2: 35,
-                    lineWidth: 1,
-                    "lineColor": "#424242"
-                }, {
-                    type: "line",
-                    x1: 40,
-                    y1: 15,
-                    x2: 140,
-                    y2: 15,
-                    lineWidth: 15,
-                    "lineColor": "#3F51B5"
-                }]
-            }, {
-                "columns": [
-                    { "text": "Eintritt", "alignment": "left" },
-                    { "text": "Datum", "alignment": "right" }
-                ],
-                "fontSize": 10,
-                "color": "#212121",
-                "margin": [0, 3, 0, 1]
-            }, {
-                "canvas": [{
-                    type: "rect",
-                    x: 0,
-                    y: 0,
-                    w: 280,
-                    h: 30,
-                    "lineColor": "#E0E0E0"
-                }, {
-                    type: "rect",
-                    x: 80,
-                    y: 0,
-                    w: 80,
-                    h: 30,
-                    "lineColor": "#E0E0E0",
-                    "color": "#EEEEEE"
-                }, {
-                    type: "line",
-                    x1: 46.66,
-                    y1: 0,
-                    x2: 46.66,
-                    y2: 30,
-                    lineWidth: 1,
-                    "lineColor": "#E0E0E0"
-                }, {
-                    type: "line",
-                    x1: 93.33,
-                    y1: 0,
-                    x2: 93.33,
-                    y2: 30,
-                    lineWidth: 1,
-                    "lineColor": "#E0E0E0"
-                }, {
-                    type: "line",
-                    x1: 140,
-                    y1: 0,
-                    x2: 140,
-                    y2: 30,
-                    lineWidth: 1,
-                    "lineColor": "#424242"
-                }, {
-                    type: "line",
-                    x1: 0,
-                    y1: 25,
-                    x2: 0,
-                    y2: 35,
-                    lineWidth: 1,
-                    "lineColor": "#9E9E9E"
-                }, {
-                    type: "line",
-                    x1: 46.66,
-                    y1: 25,
-                    x2: 46.66,
-                    y2: 35,
-                    lineWidth: 1,
-                    "lineColor": "#9E9E9E"
-                }, {
-                    type: "line",
-                    x1: 93.33,
-                    y1: 25,
-                    x2: 93.33,
-                    y2: 35,
-                    lineWidth: 1,
-                    "lineColor": "#9E9E9E"
-                }, {
-                    type: "line",
-                    x1: 140,
-                    y1: 25,
-                    x2: 140,
-                    y2: 35,
-                    lineWidth: 1,
-                    "lineColor": "#424242"
-                }, {
-                    type: "line",
-                    x1: 40,
-                    y1: 15,
-                    x2: 140,
-                    y2: 15,
-                    lineWidth: 15,
-                    "lineColor": "#3F51B5"
-                }]
-            }, {
-                "columns": [
-                    { "text": "-3", "alignment": "left" },
-                    { "text": "-2", "alignment": "left" },
-                    { "text": "-1", "alignment": "left" },
-                    { "text": "0", "alignment": "center" },
-                    { "text": "1", "alignment": "right" },
-                    { "text": "2", "alignment": "right" },
-                    { "text": "3", "alignment": "right" }
-                ],
-                "fontSize": 9,
-                "color": "#757575"
-            }]
+            "text": "Dies ist ein Standardtext",
+            "style": "text"
         }, {
-            "width": 110,
-            "fontSize": 10,
             "alignment": "left",
-            "text": "RECHTS: Dies ist ein Standardtext für die Beschreibung, diese kann auch sehr lange sein.",
-            "margin": [0, 14, 0, 0]
+            "columns": [{
+                "width": 110,
+                "fontSize": 10,
+                "alignment": "right",
+                "text": "LINKS: Dies ist ein Standardtext",
+                "margin": [0, 14, 0, 0]
+            }, {
+                "width": "auto",
+                "stack": [{
+                    "columns": [
+                        { "text": "Eintritt", "alignment": "left" },
+                        { "text": "Datum", "alignment": "right" }
+                    ],
+                    "fontSize": 10,
+                    "color": "#212121",
+                    "margin": [0, 3, 0, 1]
+                }, {
+                    canvas: [{
+                        type: "rect",
+                        x: 0,
+                        y: 0,
+                        w: 280,
+                        h: 30,
+                        "lineColor": "#E0E0E0"
+                    }, {
+                        type: "rect",
+                        x: 80,
+                        y: 0,
+                        w: 80,
+                        h: 30,
+                        "lineColor": "#E0E0E0",
+                        "color": "#EEEEEE"
+                    }, {
+                        type: "line",
+                        x1: 46.66,
+                        y1: 0,
+                        x2: 46.66,
+                        y2: 30,
+                        lineWidth: 1,
+                        "lineColor": "#E0E0E0"
+                    }, {
+                        type: "line",
+                        x1: 93.33,
+                        y1: 0,
+                        x2: 93.33,
+                        y2: 30,
+                        lineWidth: 1,
+                        "lineColor": "#E0E0E0"
+                    }, {
+                        type: "line",
+                        x1: 140,
+                        y1: 0,
+                        x2: 140,
+                        y2: 30,
+                        lineWidth: 1,
+                        "lineColor": "#424242"
+                    }, {
+                        type: "line",
+                        x1: 0,
+                        y1: 25,
+                        x2: 0,
+                        y2: 35,
+                        lineWidth: 1,
+                        "lineColor": "#9E9E9E"
+                    }, {
+                        type: "line",
+                        x1: 46.66,
+                        y1: 25,
+                        x2: 46.66,
+                        y2: 35,
+                        lineWidth: 1,
+                        "lineColor": "#9E9E9E"
+                    }, {
+                        type: "line",
+                        x1: 93.33,
+                        y1: 25,
+                        x2: 93.33,
+                        y2: 35,
+                        lineWidth: 1,
+                        "lineColor": "#9E9E9E"
+                    }, {
+                        type: "line",
+                        x1: 140,
+                        y1: 25,
+                        x2: 140,
+                        y2: 35,
+                        lineWidth: 1,
+                        "lineColor": "#424242"
+                    }, {
+                        type: "line",
+                        x1: 40,
+                        y1: 15,
+                        x2: 140,
+                        y2: 15,
+                        lineWidth: 15,
+                        "lineColor": "#3F51B5"
+                    }]
+                }, {
+                    "columns": [
+                        { "text": "Eintritt", "alignment": "left" },
+                        { "text": "Datum", "alignment": "right" }
+                    ],
+                    "fontSize": 10,
+                    "color": "#212121",
+                    "margin": [0, 3, 0, 1]
+                }, {
+                    "canvas": [{
+                        type: "rect",
+                        x: 0,
+                        y: 0,
+                        w: 280,
+                        h: 30,
+                        "lineColor": "#E0E0E0"
+                    }, {
+                        type: "rect",
+                        x: 80,
+                        y: 0,
+                        w: 80,
+                        h: 30,
+                        "lineColor": "#E0E0E0",
+                        "color": "#EEEEEE"
+                    }, {
+                        type: "line",
+                        x1: 46.66,
+                        y1: 0,
+                        x2: 46.66,
+                        y2: 30,
+                        lineWidth: 1,
+                        "lineColor": "#E0E0E0"
+                    }, {
+                        type: "line",
+                        x1: 93.33,
+                        y1: 0,
+                        x2: 93.33,
+                        y2: 30,
+                        lineWidth: 1,
+                        "lineColor": "#E0E0E0"
+                    }, {
+                        type: "line",
+                        x1: 140,
+                        y1: 0,
+                        x2: 140,
+                        y2: 30,
+                        lineWidth: 1,
+                        "lineColor": "#424242"
+                    }, {
+                        type: "line",
+                        x1: 0,
+                        y1: 25,
+                        x2: 0,
+                        y2: 35,
+                        lineWidth: 1,
+                        "lineColor": "#9E9E9E"
+                    }, {
+                        type: "line",
+                        x1: 46.66,
+                        y1: 25,
+                        x2: 46.66,
+                        y2: 35,
+                        lineWidth: 1,
+                        "lineColor": "#9E9E9E"
+                    }, {
+                        type: "line",
+                        x1: 93.33,
+                        y1: 25,
+                        x2: 93.33,
+                        y2: 35,
+                        lineWidth: 1,
+                        "lineColor": "#9E9E9E"
+                    }, {
+                        type: "line",
+                        x1: 140,
+                        y1: 25,
+                        x2: 140,
+                        y2: 35,
+                        lineWidth: 1,
+                        "lineColor": "#424242"
+                    }, {
+                        type: "line",
+                        x1: 40,
+                        y1: 15,
+                        x2: 140,
+                        y2: 15,
+                        lineWidth: 15,
+                        "lineColor": "#3F51B5"
+                    }]
+                }, {
+                    "columns": [
+                        { "text": "-3", "alignment": "left" },
+                        { "text": "-2", "alignment": "left" },
+                        { "text": "-1", "alignment": "left" },
+                        { "text": "0", "alignment": "center" },
+                        { "text": "1", "alignment": "right" },
+                        { "text": "2", "alignment": "right" },
+                        { "text": "3", "alignment": "right" }
+                    ],
+                    "fontSize": 9,
+                    "color": "#757575"
+                }]
+            }, {
+                "width": 110,
+                "fontSize": 10,
+                "alignment": "left",
+                "text": "RECHTS: Dies ist ein Standardtext für die Beschreibung, diese kann auch sehr lange sein.",
+                "margin": [0, 14, 0, 0]
+            }],
+            "columnGap": 12,
+            "margin": [0, 0, 0, 6]
         }],
-        "columnGap": 12,
-        "margin": [0, 0, 0, 6]
+        "id": "keepTogether"
     }],
     "styles": {
         "header": {
