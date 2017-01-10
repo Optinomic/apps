04372932f701410d4835d0ef65c005e6add135c1
Thomas Feron <tho.feron@gmail.com>
2017-01-10 11:24:05 +0100

    Fix view in com.optinomic.apps.whoqol

--- ch.suedhang.apps.whoqol.production/includes/WHQOL_production.sql
+++ ch.suedhang.apps.whoqol.production/includes/WHQOL_production.sql
@@ -52,7 +52,7 @@ SELECT
   SUBSTRING(((cast(response AS json))->>'submitdate'),12,5) AS submitdate_time,
   SUBSTRING(((cast(response AS json))->>'submitdate'),1,4)::integer AS submitdate_year,
   EXTRACT(WEEK FROM TO_DATE(((cast(response AS json))->>'submitdate'), 'YYYY-MM-DD HH24:MI:SS')) AS submitdate_week,
-  event.random_hash,
+  random_hash,
   scheduled,
   filled,
   module,
