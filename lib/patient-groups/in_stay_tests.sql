7cfd8dc573ce42e10ffe18bffbdddbed526ef6c9
Beat Ottiger <ottigerb@gmail.com>
2017-01-14 14:07:04 +0100

    in_stay SQL

---
+++ lib/patient-groups/in_stay_tests.sql
@@ -0,0 +1,18 @@
+
+
+SELECT 
+stay.start,
+stay.stop,
+p.* 
+FROM patient AS p
+
+
+
+LEFT JOIN stay ON(p.id = stay.patient) 
+LEFT JOIN (SELECT *, cast(value as json) AS json FROM annotations) AS ann ON ann.patient_id = p.id AND ann.module = 'com.optinomic.init.poly_stay' 
+
+WHERE 
+	(ann.json#>>'{aktuell_letzter,bel_selector,org_current}' = 'EP') 
+	AND (stay.stop is null OR stay.stop >= now())
+    AND stay.start <= now()
+
