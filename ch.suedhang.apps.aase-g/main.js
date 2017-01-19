c1cfba2ca274aee58db5652ab1fd70e35fd93658
Beat Ottiger <ottigerb@gmail.com>
2017-01-19 16:17:09 +0100

    AASE - fix

--- ch.suedhang.apps.aase-g/main.js
+++ ch.suedhang.apps.aase-g/main.js
@@ -73,7 +73,7 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
     $scope.d.navigator = 0;
 
     $scope.setCurrentResultDate = function() {
-        var date = $scope.d.dataMain.calculations[0].calculation_results[$scope.d.navigator].response.data.filled;
+        var date = $scope.d.dataMain.survey_responses[$scope.d.navigator].entity.data.filled
 
         $scope.d.dataMain.calculations[0].calculation_results[$scope.d.navigator].response.data.filled_date = {
             'filled_datestamp': date,
@@ -86,7 +86,7 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
 
 
     $scope.prev = function() {
-        var count = $scope.d.dataMain.calculations[0].calculation_results.length - 1;
+        var count = $scope.d.dataMain.survey_responses.length - 1;
 
         if ($scope.d.navigator === 0) {
             $scope.d.navigator = count;
@@ -97,7 +97,7 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
     };
 
     $scope.next = function() {
-        var count = $scope.d.dataMain.calculations[0].calculation_results.length - 1;
+        var count = $scope.d.dataMain.survey_responses.length - 1;
 
         if (count === $scope.d.navigator) {
             $scope.d.navigator = 0;
