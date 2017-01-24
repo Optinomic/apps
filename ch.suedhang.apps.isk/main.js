19d92ed68259178cbc4b2292bfef8c8724c4cfd1
Beat Ottiger <ottigerb@gmail.com>
2017-01-24 11:01:03 +0100

    undefinded == Anderer MZ

--- ch.suedhang.apps.isk/main.js
+++ ch.suedhang.apps.isk/main.js
@@ -215,16 +215,11 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
 
             // Messzeitpung
             var mz_id = messung.info.mz.mz_id;
-            if (mz_id === 99) {
+            if (mz_id === 99) || (mz_id === null) {
                 mz_id = 2; // Unbekannt => Anderer Messzeitpunkt
             } else {
                 //mz_id = mz_id;
                 mz_id = mz_id - 1;
-
-                //Prevent Error
-                if (mz_id = -1) {
-                    mz_id = 0;
-                };
             };
 
 
