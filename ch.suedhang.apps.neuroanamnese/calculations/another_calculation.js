7c5d35719f2cb9eef8c2ba315714e8665a5f9029
Beat Ottiger <ottigerb@gmail.com>
2017-02-03 17:26:53 +0100

    Calculation for ch.suedhang.apps.neuroanamnese.production

--- ch.suedhang.apps.neuroanamnese/calculations/another_calculation.js
+++ ch.suedhang.apps.neuroanamnese/calculations/another_calculation.js
@@ -1,43 +1,81 @@
 function main(responses) {
+
     var calc = {};
 
     // ------------------------------------------
     // H e l p e r   -   F U N C T I O N S
     // ------------------------------------------
 
-/*
-    calc.getNumItems = function() {
-        
+
+    calc.getNumItems = function(result) {
+
         var items = 0;
-        if (result.Erhebungszeitpunkt == 1) {
-            items = items + parseInt(result['NeuroAnamEin_SQ001']);
-            items = items + parseInt(result['NeuroAnamEin_SQ002']);
-            items = items + parseInt(result['NeuroAnamEin_SQ003']);
-            items = items + parseInt(result['NeuroAnamEin_SQ004']);
-            items = items + parseInt(result['NeuroAnamEin_SQ005']);
-            items = items + parseInt(result['NeuroAnamEin_SQ006']);
-            items = items + parseInt(result['NeuroAnamEin_SQ007']);
-            items = items + parseInt(result['NeuroAnamEin_SQ008']);
-            items = items + parseInt(result['NeuroAnamEin_SQ009']);
-            items = items + parseInt(result['NeuroAnamEin_SQ010']);
-            items = items + parseInt(result['NeuroAnamEin_SQ011']);
-            items = items + parseInt(result['NeuroAnamEin_SQ013']);
+
+
+        if ("NeuroAnamEin_SQ001" in result) {
+            // V1-Daten Version ebenfalls unterstützen
+            // Diese haben wir bei der Migration V1->V2 nicht angefasst.
+
+            if (parseInt(result.Erhebungszeitpunkt) === 1) {
+                items = items + parseInt(result['NeuroAnamEin_SQ001']);
+                items = items + parseInt(result['NeuroAnamEin_SQ002']);
+                items = items + parseInt(result['NeuroAnamEin_SQ003']);
+                items = items + parseInt(result['NeuroAnamEin_SQ004']);
+                items = items + parseInt(result['NeuroAnamEin_SQ005']);
+                items = items + parseInt(result['NeuroAnamEin_SQ006']);
+                items = items + parseInt(result['NeuroAnamEin_SQ007']);
+                items = items + parseInt(result['NeuroAnamEin_SQ008']);
+                items = items + parseInt(result['NeuroAnamEin_SQ009']);
+                items = items + parseInt(result['NeuroAnamEin_SQ010']);
+                items = items + parseInt(result['NeuroAnamEin_SQ011']);
+                items = items + parseInt(result['NeuroAnamEin_SQ013']);
+            } else {
+                items = items + parseInt(result['NeuroAnamAus_SQ001']);
+                items = items + parseInt(result['NeuroAnamAus_SQ002']);
+                items = items + parseInt(result['NeuroAnamAus_SQ003']);
+                items = items + parseInt(result['NeuroAnamAus_SQ006']);
+                items = items + parseInt(result['NeuroAnamAus_SQ007']);
+                items = items + parseInt(result['NeuroAnamAus_SQ008']);
+                items = items + parseInt(result['NeuroAnamAus_SQ009']);
+                items = items + parseInt(result['NeuroAnamAus_SQ010']);
+                items = items + parseInt(result['NeuroAnamAus_SQ011']);
+                items = items + parseInt(result['NeuroAnamAus_SQ012']);
+            };
         } else {
-            items = items + parseInt(result['NeuroAnamAus_SQ001']);
-            items = items + parseInt(result['NeuroAnamAus_SQ002']);
-            items = items + parseInt(result['NeuroAnamAus_SQ003']);
-            items = items + parseInt(result['NeuroAnamAus_SQ006']);
-            items = items + parseInt(result['NeuroAnamAus_SQ007']);
-            items = items + parseInt(result['NeuroAnamAus_SQ008']);
-            items = items + parseInt(result['NeuroAnamAus_SQ009']);
-            items = items + parseInt(result['NeuroAnamAus_SQ010']);
-            items = items + parseInt(result['NeuroAnamAus_SQ011']);
-            items = items + parseInt(result['NeuroAnamAus_SQ012']);
-        };
+            //V2-Daten Version
+            if (parseInt(result.Erhebungszeitpunkt) === 1) {
+                items = items + parseInt(result['NeuroAnamEin[SQ001]']);
+                items = items + parseInt(result['NeuroAnamEin[SQ002]']);
+                items = items + parseInt(result['NeuroAnamEin[SQ003]']);
+                items = items + parseInt(result['NeuroAnamEin[SQ004]']);
+                items = items + parseInt(result['NeuroAnamEin[SQ005]']);
+                items = items + parseInt(result['NeuroAnamEin[SQ006]']);
+                items = items + parseInt(result['NeuroAnamEin[SQ007]']);
+                items = items + parseInt(result['NeuroAnamEin[SQ008]']);
+                items = items + parseInt(result['NeuroAnamEin[SQ009]']);
+                items = items + parseInt(result['NeuroAnamEin[SQ010]']);
+                items = items + parseInt(result['NeuroAnamEin[SQ011]']);
+                items = items + parseInt(result['NeuroAnamEin[SQ012]']);
+            } else {
+                items = items + parseInt(result['NeuroAnamAus[SQ001]']);
+                items = items + parseInt(result['NeuroAnamAus[SQ002]']);
+                items = items + parseInt(result['NeuroAnamAus[SQ003]']);
+                items = items + parseInt(result['NeuroAnamAus[SQ007]']);
+                items = items + parseInt(result['NeuroAnamAus[SQ008]']);
+                items = items + parseInt(result['NeuroAnamAus[SQ009]']);
+                items = items + parseInt(result['NeuroAnamAus[SQ010]']);
+                items = items + parseInt(result['NeuroAnamAus[SQ011]']);
+                items = items + parseInt(result['NeuroAnamAus[SQ012]']);
+                items = items + parseInt(result['NeuroAnamAus[SQ013]']);
+            };
+
+        }
+
+
 
         return items;
     };
-*/
+
 
     // ------------------------------------------
     // F U N C T I O N  -  Main
@@ -52,7 +90,8 @@ function main(responses) {
             var result = response.data.response;
 
             // Something
-            myResults.something = calc.doSomething();
+            myResults.items = calc.getNumItems(result);
+            myResults.mz = parseInt(result.Erhebungszeitpunkt);
 
 
             // Write Results for the Return
@@ -68,4 +107,6 @@ function main(responses) {
 
     // Return
     return calc.getResults(responses);
+
+
 }
