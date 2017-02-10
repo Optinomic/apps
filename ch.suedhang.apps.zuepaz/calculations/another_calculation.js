cf6e981ca8fed58e1175c66e34dafcb41ed61c1a
schoenenb <nora.schoenenberger@suedhang.ch>
2017-02-10 10:16:09 +0100

    Revert "ZüPaZ: keine Angabe in calculations"
    This reverts commit c8d61118f09afef220705e7029b3472c3fb2556e.

--- ch.suedhang.apps.zuepaz/calculations/another_calculation.js
+++ ch.suedhang.apps.zuepaz/calculations/another_calculation.js
@@ -16,107 +16,40 @@ function main(responses) {
         var full_score = 0;
 
         var anz_anworten_eintritt = 2;
-        if (d.Q00001 === "999") {
-            anz_anworten_eintritt = anz_anworten_eintritt - 1;
-        } else {
         full_score = full_score + parseInt(d.Q00001);
-
-        if (d.Q00002 === "999") {
-            anz_anworten_eintritt = anz_anworten_eintritt - 1;
-        } else {
         full_score = full_score + parseInt(d.Q00002);
-
         var zupaz_eintritt_score = full_score;
         var zupaz_eintritt_mean = calc.roundToOne(full_score / anz_anworten_eintritt);
 
         var anz_anworten_aufenthalt = 3;
-        if (d.Q00003 === "999") {
-            anz_anworten_aufenthalt = anz_anworten_aufenthalt - 1;
-        } else {
         full_score = full_score + parseInt(d.Q00003);
-
-        if (d.Q00004 === "999") {
-            anz_anworten_aufenthalt = anz_anworten_aufenthalt - 1;
-        } else {
         full_score = full_score + parseInt(d.Q00004);
-
-        if (d.Q00005 === "999") {
-            anz_anworten_aufenthalt = anz_anworten_aufenthalt - 1;
-        } else {
         full_score = full_score + parseInt(d.Q00005);
         var zupaz_aufenthalt_score = full_score - zupaz_eintritt_score;
         var zupaz_aufenthalt_mean = calc.roundToOne(zupaz_aufenthalt_score / anz_anworten_aufenthalt);
 
         var anz_anworten_behandlung = 19;
-        if (d.Q00006 === "999") {
-            anz_anworten_behandlung = anz_anworten_behandlung - 1;
-        } else {        
         full_score = full_score + parseInt(d.Q00006);
-
-        if (d.Q00007 === "999") {
-            anz_anworten_behandlung = anz_anworten_behandlung - 1;
-        } else {  
         full_score = full_score + parseInt(d.Q00007);
-
-        if (d.Q00008 === "999") {
-            anz_anworten_behandlung = anz_anworten_behandlung - 1;
-        } else {  
         full_score = full_score + parseInt(d.Q00008);
-
-        if (d.Q00009 === "999") {
-            anz_anworten_behandlung = anz_anworten_behandlung - 1;
-        } else {  
         full_score = full_score + parseInt(d.Q00009);
-
-        if (d.Q00010 === "999") {
-            anz_anworten_behandlung = anz_anworten_behandlung - 1;
-        } else {  
         full_score = full_score + parseInt(d.Q00010);
-
-        if (d.Q00011 === "999") {
-            anz_anworten_behandlung = anz_anworten_behandlung - 1;
-        } else {  
         full_score = full_score + parseInt(d.Q00011);
-
-        if (d.Q00012 === "999") {
-            anz_anworten_behandlung = anz_anworten_behandlung - 1;
-        } else {  
         full_score = full_score + parseInt(d.Q00012);
-
-        if (d.Q00013 === "999") {
-            anz_anworten_behandlung = anz_anworten_behandlung - 1;
-        } else {  
         full_score = full_score + parseInt(d.Q00013);
-
-        if (d.Q00014 === "999") {
-            anz_anworten_behandlung = anz_anworten_behandlung - 1;
-        } else {  
         full_score = full_score + parseInt(d.Q00014);
-
-        if (d.Q00015 === "999") {
-            anz_anworten_behandlung = anz_anworten_behandlung - 1;
-        } else {  
         full_score = full_score + parseInt(d.Q00015);
-
-        if (d.Q00016 === "999") {
-            anz_anworten_behandlung = anz_anworten_behandlung - 1;
-        } else {  
         full_score = full_score + parseInt(d.Q00016);
-
-        if (d.Q00017 === "999") {
-            anz_anworten_behandlung = anz_anworten_behandlung - 1;
-        } else {  
         full_score = full_score + parseInt(d.Q00017);
 
-
-        if (d.Q00018 === "kA" || d.Q00018 === "999") {
+        if (d.Q00018 === "kA") {
             anz_anworten_behandlung = anz_anworten_behandlung - 1;
         } else {
             full_score = full_score + parseInt(d.Q00018);
         };
 
 
-        if (d.Q00019 === "kA" || d.Q00019 ==="999") {
+        if (d.Q00019 === "kA") {
             anz_anworten_behandlung = anz_anworten_behandlung - 1;
         } else {
             if (parseInt(d.Q00019) === 0) {
@@ -138,49 +71,20 @@ function main(responses) {
                 full_score = full_score + 0;
             }
         };
-        if (d.Q00020 === "999") {
-            anz_anworten_behandlung = anz_anworten_behandlung - 1;
-        } else {
+
         full_score = full_score + parseInt(d.Q00020);
-
-        if (d.Q00021 === "999") {
-            anz_anworten_behandlung = anz_anworten_behandlung - 1;
-        } else {
         full_score = full_score + parseInt(d.Q00021);
-
-        if (d.Q00022 === "999") {
-            anz_anworten_behandlung = anz_anworten_behandlung - 1;
-        } else {
         full_score = full_score + parseInt(d.Q00022);
-
-        if (d.Q00023 === "999") {
-            anz_anworten_behandlung = anz_anworten_behandlung - 1;
-        } else {
         full_score = full_score + parseInt(d.Q00023);
-
-        if (d.Q00024 === "999") {
-            anz_anworten_behandlung = anz_anworten_behandlung - 1;
-        } else {
         full_score = full_score + parseInt(d.Q00024);
-
         var zupaz_behandlung_score = full_score - zupaz_eintritt_score - zupaz_aufenthalt_score;
         var zupaz_behandlung_mean = calc.roundToOne(zupaz_behandlung_score / anz_anworten_behandlung);
 
 
         var anz_anworten_austritt = 6;
-        if (d.Q00025 === "999") {
-            anz_anworten_austritt = anz_anworten_austritt - 1;
-        } else {
         full_score = full_score + parseInt(d.Q00025);
-
-        if (d.Q00026 === "999") {
-            anz_anworten_austritt = anz_anworten_austritt - 1;
-        } else {
         full_score = full_score + parseInt(d.Q00026);
 
-        if (d.Q00027 === "999") {
-            anz_anworten_austritt = anz_anworten_austritt - 1;
-        } else {
         if (parseInt(d.Q00027) === 0) {
             full_score = full_score + 0;
         }
@@ -200,21 +104,9 @@ function main(responses) {
             full_score = full_score + 0;
         }
 
-        if (d.Q00028 === "999") {
-            anz_anworten_austritt = anz_anworten_austritt - 1;
-        } else {
         full_score = full_score + parseInt(d.Q00028);
-
-        if (d.Q00029 === "999") {
-            anz_anworten_austritt = anz_anworten_austritt - 1;
-        } else {
         full_score = full_score + parseInt(d.Q00029);
-
-        if (d.Q00030 === "999") {
-            anz_anworten_austritt = anz_anworten_austritt - 1;
-        } else {
         full_score = full_score + parseInt(d.Q00030);
-
         var zupaz_austritt_score = full_score - zupaz_eintritt_score - zupaz_aufenthalt_score - zupaz_behandlung_score;
         var zupaz_austritt_mean = calc.roundToOne(zupaz_austritt_score / anz_anworten_austritt);
 
