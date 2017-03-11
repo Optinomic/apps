4928a52cd63b9e174c10d32d52f40c40e03b09ce
Beat Ottiger <ottigerb@gmail.com>
2017-03-11 13:26:26 +0100

    wip

--- org.optinomic.apps.optinomic_redux_store/data.js
+++ org.optinomic.apps.optinomic_redux_store/data.js
@@ -1,7 +1,10 @@
 // loadData
+
+setRequestGetSurveyResponses('ch.suedhang.apps.bdi');
+
 this.dispatch('actionGetCurrentPatient');
 this.dispatch('actionGetCurrentPatientStays');
-this.dispatch('actionGetSurveyResponses("ch.suedhang.apps.bscl_anq")');
+this.dispatch('actionGetSurveyResponses');
 this.dispatch('actionGetCurrentUser');
 this.dispatch('actionGetClinic');
 this.dispatch('actionGetApps');
