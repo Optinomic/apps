3749c54f36cf4f46562c241ed1a897655ecad842
schoenenb <nora.schoenenberger@suedhang.ch>
2017-04-28 16:03:10 +0200

    TMT Export - minor change

--- ch.suedhang.apps.tmt.production/includes/export_production.sql
+++ ch.suedhang.apps.tmt.production/includes/export_production.sql
@@ -9,6 +9,7 @@ SELECT
   ((cast(response AS json))->>'id') as optinomic_limesurvey_id,
   -- END:  Optinoimc Default |  Needed for Export-Toolbox
 
+  CONCAT(patient.cis_pid, '00', RIGHT((stay.cis_fid/100)::text,2)) as MedStatFid,
   stay.cis_fid/100 as FID,
   TO_DATE(TO_CHAR((filled), 'YYYY-MM-DD'), 'YYYY-MM-DD') as datum_filled,
   ((cast(response AS json))->>'Date') as Datum_alt,
@@ -21,30 +22,6 @@ SELECT
   ((cast(response AS json))->>'TMTBError') as FehlerB,
   ((cast(response AS json))->>'BzWert') as zWertB_alt,
   ((cast(response AS json))->>'BAzWert') as zWertQ_alt
-  /*
-  ,TO_DATE(((cast(response AS json))->>'Date'), 'YYYY-MM-DD HH24:MI:SS')  as datum_date,
-  SUBSTRING(((cast(response AS json))->>'Date'),12,5) AS datum_time,
-  SUBSTRING(((cast(response AS json))->>'Date'),1,4)::integer AS datum_year,
-  EXTRACT(WEEK FROM TO_DATE(((cast(response AS json))->>'Date'), 'YYYY-MM-DD HH24:MI:SS')) AS datum_week,
-  ((cast(response AS json))->>'Messzeitpunkt') as erhebungszeitpunkt,
-  ((cast(response AS json))->>'FID') as fid_survey,
-  ((cast(response AS json))->>'PID') as pid,
-  ((cast(response AS json))->>'datestamp') as datestamp,
-  TO_DATE(((cast(response AS json))->>'datestamp'), 'YYYY-MM-DD HH24:MI:SS')  as datestamp_date,
-  SUBSTRING(((cast(response AS json))->>'datestamp'),12,5) AS datestamp_time,
-  SUBSTRING(((cast(response AS json))->>'datestamp'),1,4)::integer AS datestamp_year,
-  EXTRACT(WEEK FROM TO_DATE(((cast(response AS json))->>'datestamp'), 'YYYY-MM-DD HH24:MI:SS')) AS datestamp_week,
-  ((cast(response AS json))->>'id') as id,
-  ((cast(response AS json))->>'lastpage') as lastpage,
-  ((cast(response AS json))->>'optinomixHASH') as optinomixhash,
-  ((cast(response AS json))->>'startdate') as startdate,
-  TO_DATE(((cast(response AS json))->>'startdate'), 'YYYY-MM-DD HH24:MI:SS')  as startdate_date,
-  SUBSTRING(((cast(response AS json))->>'startdate'),12,5) AS startdate_time,
-  SUBSTRING(((cast(response AS json))->>'startdate'),1,4)::integer AS startdate_year,
-  EXTRACT(WEEK FROM TO_DATE(((cast(response AS json))->>'startdate'), 'YYYY-MM-DD HH24:MI:SS')) AS startdate_week,
-  ((cast(response AS json))->>'startlanguage') as startlanguage,
-  ((cast(response AS json))->>'submitdate') as submitdate
-*/
 
 FROM "survey_response_view" 
 LEFT JOIN patient ON(survey_response_view.patient_id = patient.id) 
