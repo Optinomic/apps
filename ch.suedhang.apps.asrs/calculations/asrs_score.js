636e86323be50d48d57788e423e5b72fe4a4f0b5
schoenenb <nora.schoenenberger@suedhang.ch>
2017-05-05 11:13:41 +0200

    ASRS: calculation

---
+++ ch.suedhang.apps.asrs/calculations/asrs_score.js
@@ -0,0 +1,92 @@
+function main(responses) {
+    var calc = {};
+
+    // ------------------------------------------
+    // H e l p e r   -   F U N C T I O N S
+    // ------------------------------------------
+
+    calc.bdiScore = function(result) {
+
+        var score = 0;
+
+        if (ASRS_1 > 2) {
+        	score = score + 1;
+        };
+        if (ASRS_2 > 2) {
+        	score = score + 1;
+        };
+        if (ASRS_3 > 2) {
+        	score = score + 1;
+        };
+        	score = score + 1;
+        };
+        	score = score + 1;
+        };
+        	score = score + 1;
+        };
+        
+
+        var current_range = {};
+
+        var ranges = [{
+            "from": 0,
+            "to": 2,
+            "interpretation_de": "Keine Hinweise auf ADHS",
+            "result_color": "#4CAF50"
+        }, {
+            "from": 20,
+            "to": 28,
+            "interpretation_de": "Hinweise auf ADHS vorhanden",
+            "result_color": "#FF5722"
+        }];
+
+        current_range = ranges[0];
+
+        if (score >= ranges[1].from) {
+            current_range = ranges[1];
+        };
+        if (score >= ranges[2].from) {
+            current_range = ranges[2];
+        };
+
+
+        var return_obj = {
+            "score": score,
+            "current_range": current_range,
+            "ranges": ranges
+        };
+
+        return return_obj;
+    };
+
+
+    // ------------------------------------------
+    // F U N C T I O N  -  Main
+    // ------------------------------------------
+    calc.getResults = function(myResponses) {
+
+        var responses_array = myResponses.survey_responses;
+        var allResults = [];
+
+        responses_array.forEach(function(response, myindex) {
+            var myResults = {};
+            var result = response.data.response;
+
+            // BDI-Score
+            myResults.score = calc.bdiScore(result);
+
+            // Write Results for the Return
+            // Do not modify stuff here
+            myResults.hash = result['optinomixHASH'];
+            myResults.response = response;
+            // myResults.full_responses = myResponses;
+            allResults.push(myResults);
+        });
+
+        return allResults;
+    };
+
+
+    // Return
+    return calc.getResults(responses);
+}
