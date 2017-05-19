d61c8c05cb83a10102e24e151e0709fd4451ef9b
Beat Ottiger <ottigerb@gmail.com>
2017-05-19 15:33:39 +0200

    Südhang : PG's

---
+++ lib/patient-groups/Suedhang/pg_test.sql
@@ -0,0 +1,2 @@
+SELECT p.* FROM patient AS p
+LEFT JOIN stay ON(p.id = stay.patient) LEFT JOIN (SELECT *, cast(value as json) AS json FROM annotations) AS ann ON ann.patient_id = p.id AND ann.module = 'com.optinomic.init.poly_stay' WHERE (ann.json#>>'{aktuell_letzter,bel_selector,org_current}' = 'EP') AND (stay.stop is null OR stay.stop >= (now() - interval '1 day')) AND stay.start <= now()
