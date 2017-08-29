ff43161ab7a3c376d76f5be6cef8173997d32e65
Beat Ottiger <ottigerb@gmail.com>
2017-08-29 15:00:08 +0200

    SCI - Definition

---
+++ ch.suedhang.apps.fallkonferenz/definitions/sci.js
@@ -0,0 +1,188 @@
+d.sci_stress = {
+    "options": {
+        "min": 0,
+        "max": 10,
+        "item_height": 50,
+        "item_text_left": 200,
+        "item_text_right": 200,
+        "color_grid": "#9E9E9E",
+        "color_clinic_sample": "#888888",
+        "color_skin": "grey_dark_to_light",
+        "show_baseline": true,
+        "show_scale_text": true,
+        "show_score_vertical_line": false,
+        "show_score_profile_line": true,
+        "show_score_circles": true,
+        "show_settings_block": true,
+        "range_alpha": 0.1,
+        "vertical_grid_every_x": 1,
+        "response_title_path": "info.app_zeitpunkt",
+        "response_date_path": "date"
+    },
+    "scales": [{
+        "left_title": "Negative Ereignisse",
+        "left_text": "Keine oder wenig Belastung durch negative Ereignisse",
+        "right_title": "Negative Ereignisse",
+        "right_text": "Viele Stressoren durch negative Ereignisse",
+        "score_path": "info.score_0",
+        "clinic_sample_var": null
+    }, {
+        "left_title": "Stresssymptome",
+        "left_text": "Wenig körperliche und psychische Stressreaktionen",
+        "right_title": "Stresssymptome",
+        "right_text": "Viele körperliche und psychische Stressreaktionen",
+        "score_path": "info.score_1",
+        "clinic_sample_var": null
+    }],
+    "ranges": [{
+        "range_start": 0.5,
+        "range_stop": 1.5,
+        "text": "Stanine: 1 | 4%",
+        "color": "#2E7D32"
+    }, {
+        "range_start": 1.5,
+        "range_stop": 2.5,
+        "text": "Stanine: 2 | 7%",
+        "color": "#2E7D32"
+    }, {
+        "range_start": 2.5,
+        "range_stop": 3.5,
+        "text": "Stanine: 3 | 12%",
+        "color": "#FFA000"
+    }, {
+        "range_start": 3.5,
+        "range_stop": 4.5,
+        "text": "Stanine: 4 | 17%",
+        "color": "#FFA000"
+    }, {
+        "range_start": 4.5,
+        "range_stop": 5.5,
+        "text": "Stanine: 5 | 20%",
+        "color": "#FBB100"
+    }, {
+        "range_start": 5.5,
+        "range_stop": 6.5,
+        "text": "Stanine: 6 | 17%",
+        "color": "#FBB100"
+    }, {
+        "range_start": 6.5,
+        "range_stop": 7.5,
+        "text": "Stanine: 7 | 12%",
+        "color": "#FB7200"
+    }, {
+        "range_start": 7.5,
+        "range_stop": 8.5,
+        "text": "Stanine: 8 | 7%",
+        "color": "#FB7200"
+    }, {
+        "range_start": 8.5,
+        "range_stop": 9.5,
+        "text": "Stanine: 9 | 4%",
+        "color": "#C62828"
+    }]
+};
+
+d.sci_hilfreich = {
+    "options": {
+        "min": 0,
+        "max": 10,
+        "item_height": 50,
+        "item_text_left": 200,
+        "item_text_right": 200,
+        "color_grid": "#9E9E9E",
+        "color_clinic_sample": "#888888",
+        "color_skin": "grey_dark_to_light",
+        "show_baseline": true,
+        "show_scale_text": true,
+        "show_score_vertical_line": false,
+        "show_score_profile_line": true,
+        "show_score_circles": true,
+        "show_settings_block": true,
+        "range_alpha": 0.1,
+        "vertical_grid_every_x": 1,
+        "response_title_path": "info.app_zeitpunkt",
+        "response_date_path": "date"
+    },
+    "scales": [{
+        "left_title": "Positives Denken",
+        "left_text": "Selbstzweifel und Fokus auf Negatives",
+        "right_title": "Positives Denken",
+        "right_text": "Stressbewältigung durch positives Denken",
+        "score_path": "info.score_2",
+        "clinic_sample_var": null
+    }, {
+        "left_title": "Aktive Stressbewältigung",
+        "left_text": "Stressoren werden nicht beseitigt",
+        "right_title": "Aktive Stressbewältigung",
+        "right_text": "Aktive und vorbeugende Stressbewältigung",
+        "score_path": "info.score_3",
+        "clinic_sample_var": null
+    }, {
+        "left_title": "Soziale Unterstützung",
+        "left_text": "Kaum Unterstützung durch andere",
+        "right_title": "Soziale Unterstützung",
+        "right_text": "Viel Unterstützung durch Freunde und Bekannte",
+        "score_path": "info.score_4",
+        "clinic_sample_var": null
+    }, {
+        "left_title": "Halt im Glauben",
+        "left_text": "Kaum religiöser / spiritueller Halt",
+        "right_title": "Halt im Glauben",
+        "right_text": "Person findet Halt im Glauben",
+        "score_path": "info.score_5",
+        "clinic_sample_var": null
+    }, {
+        "left_title": "Alkohol- und Zigarettenkonsum",
+        "left_text": "Ungünstige Bewältigung durch Alkohol und Zigaretten",
+        "right_title": "Alkohol- und Zigarettenkonsum",
+        "right_text": "Kein erhöhter Akohol- oder Zigarettenkonsum",
+        "score_path": "info.score_6",
+        "clinic_sample_var": null
+    }],
+    "ranges": [{
+        "range_start": 0.5,
+        "range_stop": 1.5,
+        "text": "Stanine: 1 | 4%",
+        "color": "#C62828"
+    }, {
+        "range_start": 1.5,
+        "range_stop": 2.5,
+        "text": "Stanine: 2 | 7%",
+        "color": "#FB7200"
+    }, {
+        "range_start": 2.5,
+        "range_stop": 3.5,
+        "text": "Stanine: 3 | 12%",
+        "color": "#FFA000"
+    }, {
+        "range_start": 3.5,
+        "range_stop": 4.5,
+        "text": "Stanine: 4 | 17%",
+        "color": "#FBB100"
+    }, {
+        "range_start": 4.5,
+        "range_stop": 5.5,
+        "text": "Stanine: 5 | 20%",
+        "color": "#2E7D32"
+    }, {
+        "range_start": 5.5,
+        "range_stop": 6.5,
+        "text": "Stanine: 6 | 17%",
+        "color": "#2E7D32"
+    }, {
+        "range_start": 6.5,
+        "range_stop": 7.5,
+        "text": "Stanine: 7 | 12%",
+        "color": "#2E7D32"
+    }, {
+        "range_start": 7.5,
+        "range_stop": 8.5,
+        "text": "Stanine: 8 | 7%",
+        "color": "#2E7D32"
+    }, {
+        "range_start": 8.5,
+        "range_stop": 9.5,
+        "text": "Stanine: 9 | 4%",
+        "color": "#2E7D32"
+    }]
+};
