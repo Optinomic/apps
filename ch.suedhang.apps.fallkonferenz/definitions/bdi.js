383ff333f9146b3b2c56e36ce66265e5a89dfc8d
Beat Ottiger <ottigerb@gmail.com>
2017-09-11 12:09:08 +0200

    bdi - indigo_grey_pink

--- ch.suedhang.apps.fallkonferenz/definitions/bdi.js
+++ ch.suedhang.apps.fallkonferenz/definitions/bdi.js
@@ -7,7 +7,7 @@ d.bdi = {
         "item_text_right": 130,
         "color_grid": "#9E9E9E",
         "color_clinic_sample": "#888888",
-        "color_skin": "grey_dark_to_light",
+        "color_skin": "indigo_grey_pink",
         "show_baseline": false,
         "show_scale_text": false,
         "show_score_vertical_line": true,
