d982e19c02f59106d62a49d73715ea7aa620e073
Beat Ottiger <ottigerb@gmail.com>
2017-09-12 16:02:14 +0200

    BSCL Reihenfolge

--- ch.suedhang.apps.fallkonferenz/definitions/bscl.js
+++ ch.suedhang.apps.fallkonferenz/definitions/bscl.js
@@ -22,53 +22,42 @@ d.bscl = {
         "left_title": "Somatisierung",
         "left_text": "",
         "right_title": "",
-        "right_text": "Kopfschmerzen, Herzbeschwerden, Atemprobleme, Magenbeschwerden, Muskelschmerzen,     Schwächegefühl, Schweregefühl, Unwohlsein usw.",
+        "right_text": "Kopfschmerzen, Herzbeschwerden, Atemprobleme, Magenbeschwerden, Muskelschmerzen, Schwächegefühl, Schweregefühl, Unwohlsein usw.",
         "m_norm": [0.23, 0.32],
         "sd_norm": [0.31, 0.33],
         "score_path": "calculation.scores_calculation.all_results.somatisierung_z_score",
         "clinic_sample_var": "somatisierung_z_score",
         "items": 7
     }, {
-        "id": 1,
-        "left_title": "Psychotizismus",
+        "left_title": "Zwanghaftigkeit",
         "left_text": "",
         "right_title": "",
-        "right_text": "Gefühl der Isolation und zwischenmenschlichen Entfremdung. Verzerrter, isolierter Lebensstil     bis zu Halluzination und Gedankenzerfall.",
-        "m_norm": [0.19, 0.19],
-        "sd_norm": [0.28, 0.27],
-        "score_path": "calculation.scores_calculation.all_results.psychotizismus_z_score",
-        "clinic_sample_var": "psychotizismus_z_score",
-        "items": 5
+        "right_text": "Gedanken, Impulse und Handlungen, die konstant vorhanden und nicht änderbar und ich-fremd oder ungewollt erlebt werden, Kognitive Leistungsstörungen.",
+        "m_norm": [0.50, 0.54],
+        "sd_norm": [0.46, 0.43],
+        "score_path": "calculation.scores_calculation.all_results.zwanghaftigkeit_z_score",
+        "clinic_sample_var": "zwanghaftigkeit_z_score",
+        "items": 6
     }, {
-        "left_title": "Paranoides Denken",
+        "left_title": "Unsicherheit im Sozialkontakt",
         "left_text": "",
         "right_title": "",
-        "right_text": "Misstrauen, Minderwertigkeitsgefühle, paranoides Denken: Gedankenprojektion, Feindseligkeit,     Argwohn, Grandiosität, Einengung, Angst vor Autonomieverlust und wahnhafte Täuschung.",
-        "m_norm": [0.33, 0.34],
-        "sd_norm": [0.40, 0.38],
-        "score_path": "calculation.scores_calculation.all_results.paranoides_denken_z_score",
-        "clinic_sample_var": "paranoides_denken_z_score",
-        "items": 5
+        "right_text": "Unzulänglichkeits- und Minderwertigkeitsgefühle, Selbstabwertungen im sozialen Kontakt, Selbstzweifel, Selbstunsicherheit und negative Erwartungen bzgl. dem eigenen zwischenmenschlichen   Verhalten.",
+        "m_norm": [0.35, 0.49],
+        "sd_norm": [0.40, 0.45],
+        "score_path": "calculation.scores_calculation.all_results.unsicherheit_im_sozialkontakt_z_score",
+        "clinic_sample_var": "unsicherheit_im_sozialkontakt_z_score",
+        "items": 4
     }, {
-        "left_title": "Phobische Angst",
+        "left_title": "Depressivität",
         "left_text": "",
         "right_title": "",
-        "right_text": "Andauernde und unangemessene Furcht als Reaktion auf eine bestimmte Person, einen Platz, ein     Objekt oder eine charakteristische Situation, die zu Vermeidungs- oder Fluchtverhalten führt.",
-        "m_norm": [0.14, 0.16],
-        "sd_norm": [0.23, 0.25],
-        "score_path": "calculation.scores_calculation.all_results.phobische_angst_z_score",
-        "clinic_sample_var": "phobische_angst_z_score",
-        "items": 5
-    }, {
-        "left_title": "Aggressivität / Feindseligkeit",
-        "left_text": "",
-        "right_title": "",
-        "right_text": "Reizbarkeit und Unausgeglichenheit bis hin zu starker Aggressivität. Ärger, Aggression,    Irritierbarkeit, Zorn und Verstimmung.",
-        "m_norm": [0.29, 0.37],
-        "sd_norm": [0.35, 0.33],
-        "score_path": "calculation.scores_calculation.all_results.aggressivit__t___feindseligkeit_z_score",
-        "clinic_sample_var": "aggressivit__t___feindseligkeit_z_score",
-        "items": 5
+        "right_text": "Gedrückte Stimmung, Gesunkenes Interesse am Leben, Verringerte Motivation und Energie, Hoffnungslosigkeit, bis hin zu Suizidgedanken.",
+        "m_norm": [0.24, 0.33],
+        "sd_norm": [0.32, 0.40],
+        "score_path": "calculation.scores_calculation.all_results.depressivit__t_z_score",
+        "clinic_sample_var": "depressivit__t_z_score",
+        "items": 6
     }, {
         "left_title": "Ängstlichkeit",
         "left_text": "",
@@ -80,35 +69,45 @@ d.bscl = {
         "clinic_sample_var": "__ngstlichkeit_z_score",
         "items": 6
     }, {
-        "left_title": "Depressivität",
+        "left_title": "Aggressivität / Feindseligkeit",
         "left_text": "",
         "right_title": "",
-        "right_text": "Gedrückte Stimmung, Gesunkenes Interesse am Leben, Verringerte Motivation und Energie,     Hoffnungslosigkeit, bis hin zu Suizidgedanken.",
-        "m_norm": [0.24, 0.33],
-        "sd_norm": [0.32, 0.40],
-        "score_path": "calculation.scores_calculation.all_results.depressivit__t_z_score",
-        "clinic_sample_var": "depressivit__t_z_score",
-        "items": 6
+        "right_text": "Reizbarkeit und Unausgeglichenheit bis hin zu starker Aggressivität. Ärger, Aggression, Irritierbarkeit, Zorn und Verstimmung.",
+        "m_norm": [0.29, 0.37],
+        "sd_norm": [0.35, 0.33],
+        "score_path": "calculation.scores_calculation.all_results.aggressivit__t___feindseligkeit_z_score",
+        "clinic_sample_var": "aggressivit__t___feindseligkeit_z_score",
+        "items": 5
     }, {
-        "left_title": "Unsicherheit im Sozialkontakt",
+        "left_title": "Phobische Angst",
         "left_text": "",
         "right_title": "",
-        "right_text": "Unzulänglichkeits- und Minderwertigkeitsgefühle, Selbstabwertungen im sozialen Kontakt,    Selbstzweifel, Selbstunsicherheit und negative Erwartungen bzgl. dem eigenen zwischenmenschlichen   Verhalten.",
-        "m_norm": [0.35, 0.49],
-        "sd_norm": [0.40, 0.45],
-        "score_path": "calculation.scores_calculation.all_results.unsicherheit_im_sozialkontakt_z_score",
-        "clinic_sample_var": "unsicherheit_im_sozialkontakt_z_score",
-        "items": 4
+        "right_text": "Andauernde und unangemessene Furcht als Reaktion auf eine Person, einen Platz, ein Objekt oder eine Situation, die zu Vermeidungs- oder Fluchtverhalten führt.",
+        "m_norm": [0.14, 0.16],
+        "sd_norm": [0.23, 0.25],
+        "score_path": "calculation.scores_calculation.all_results.phobische_angst_z_score",
+        "clinic_sample_var": "phobische_angst_z_score",
+        "items": 5
     }, {
-        "left_title": "Zwanghaftigkeit",
+        "left_title": "Paranoides Denken",
         "left_text": "",
         "right_title": "",
-        "right_text": "Gedanken, Impulse und Handlungen, die konstant vorhanden und nicht änderbar und ich-fremd oder ungewollt erlebt werden, Kognitive Leistungsstörungen.",
-        "m_norm": [0.50, 0.54],
-        "sd_norm": [0.46, 0.43],
-        "score_path": "calculation.scores_calculation.all_results.zwanghaftigkeit_z_score",
-        "clinic_sample_var": "zwanghaftigkeit_z_score",
-        "items": 6
+        "right_text": "Misstrauen, Minderwertigkeitsgefühle, paranoides Denken: Gedankenprojektion, Feindseligkeit, Einengung, Angst vor Autonomieverlust und wahnhafte Täuschung.",
+        "m_norm": [0.33, 0.34],
+        "sd_norm": [0.40, 0.38],
+        "score_path": "calculation.scores_calculation.all_results.paranoides_denken_z_score",
+        "clinic_sample_var": "paranoides_denken_z_score",
+        "items": 5
+    }, {
+        "left_title": "Psychotizismus",
+        "left_text": "",
+        "right_title": "",
+        "right_text": "Gefühl der Isolation und zwischenmenschlichen Entfremdung. Verzerrter, isolierter Lebensstil bis zu Halluzination und Gedankenzerfall.",
+        "m_norm": [0.19, 0.19],
+        "sd_norm": [0.28, 0.27],
+        "score_path": "calculation.scores_calculation.all_results.psychotizismus_z_score",
+        "clinic_sample_var": "psychotizismus_z_score",
+        "items": 5
     }, {
         "left_title": "GSI",
         "left_text": "Global Severity Index",
