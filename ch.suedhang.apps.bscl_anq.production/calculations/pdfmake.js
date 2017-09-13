be2802b00133d9e381483e17bb3aebf212fd49b2
Beat Ottiger <ottigerb@gmail.com>
2017-09-13 20:07:17 +0200

    "version": 0

--- ch.suedhang.apps.bscl_anq.production/calculations/pdfmake.js
+++ ch.suedhang.apps.bscl_anq.production/calculations/pdfmake.js
@@ -1,8 +1,11 @@
 function main(responses) {
 
- 
    // "JavascriptError "JS execution failed: undefined:1\nundefined\n^\n\nSyntaxError: Unexpected token u\n    at Object.parse (native)\n    at Object.calc.sort_scores ([stdin]:1396:32)\n    at Object.calc.getResults ([stdin]:1423:27)\n    at main ([stdin]:1501:21)\n    at [stdin]:1506:28\n    at Object.exports.runInThisContext (vm.js:54:17)\n    at Object.<anonymous> ([stdin]-wrapper:6:22)\n    at Module._compile (module.js:410:26)\n    at node.js:578:27\n    at nextTickCallbackWith0Args (node.js:419:9)\n
 
-   return responses.other_calculations;
+   var nothing = {
+        "version": 0
+   };
+
+   return nothing;
 
 };
