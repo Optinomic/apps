109a88fd69066b01589b62b36f205bc856810213
Beat Ottiger <ottigerb@gmail.com>
2017-09-13 19:38:47 +0200

    wip

--- ch.suedhang.apps.bscl_anq.production/calculations/pdfmake.js
+++ ch.suedhang.apps.bscl_anq.production/calculations/pdfmake.js
@@ -1,5 +1,8 @@
 function main(responses) {
 
-    return responses;
+    var calc = {};
+    calc.master = 'ch.suedhang.apps.bscl_anq.production:scores_calculation';
+
+    include(../ch.suedhang.apps.bscl_anq/calculations/pdfmake_global.js)
 
 };
