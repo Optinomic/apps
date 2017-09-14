a35f8192ed124cd806f276b67e3fbe356f6835a4
schoenenb <nora.schoenenberger@suedhang.ch>
2017-09-14 15:08:42 +0200

    Ranges AASE -0.5 - 4.5
    Da Werte von 0-4 möglich

--- ch.suedhang.apps.fallkonferenz.production/definitions/aase.js
+++ ch.suedhang.apps.fallkonferenz.production/definitions/aase.js
@@ -1,7 +1,7 @@
 d.aase = {
     "options": {
-        "min": 0,
-        "max": 4,
+        "min": -0.5,
+        "max": 4.5,
         "item_height": 50,
         "item_text_left": 135,
         "item_text_right": 135,
