ea317f19c38bc34c4d890039b93ef2e664ccfea8
schoenenb <nora.schoenenberger@suedhang.ch>
2017-09-22 12:22:59 +0200

    hide_first_last für ASRS

--- ch.suedhang.apps.fallkonferenz/definitions/asrs.js
+++ ch.suedhang.apps.fallkonferenz/definitions/asrs.js
@@ -15,6 +15,7 @@ d.asrs = {
         "show_score_circles": true,
         "show_settings_block": false,
         "allow_toggle_settings_block": true,
+        "topnumber_hide_first_last": true,
         "range_alpha": 0.09,
         "vertical_grid_every_x": 1,
         "response_title_path": "calculation.asrs_score.score.current_range.interpretation",
