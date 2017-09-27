397cb72962e7a261317fa1e4eec28b0c8165e8a1
Beat Ottiger <ottigerb@gmail.com>
2017-09-27 18:38:16 +0200

    TOC

---
+++ ch.suedhang.apps.fallkonferenz/_data_toc.js
@@ -0,0 +1,41 @@
+d.toc = [{
+    "name": "act-Info",
+    "anchor": "content_actinfo"
+}, {
+    "name": "TMT",
+    "anchor": "content_tmt"
+}, {
+    "name": "ASRS",
+    "anchor": "content_asrs"
+}, {
+    "name": "AASE",
+    "anchor": "content_aase"
+}, {
+    "name": "BSCL",
+    "anchor": "content_bscl"
+}, {
+    "name": "BDI-II",
+    "anchor": "content_bdi"
+}, {
+    "name": "ISK",
+    "anchor": "content_isk"
+}, {
+    "name": "SCI",
+    "anchor": "content_sci"
+}, {
+    "name": "WHOQOL",
+    "anchor": "content_whoqol"
+}];
+
+d.data_app = {
+    "actinfo_ein": "ch.suedhang.apps.actinfo_ein",
+    "actinfo_aus": "ch.suedhang.apps.actinfo_aus",
+    "tmt": "ch.suedhang.apps.tmt_V3",
+    "asrs": "ch.suedhang.apps.asrs",
+    "aase": "ch.suedhang.apps.aase-g",
+    "bscl": "ch.suedhang.apps.bscl_anq",
+    "bdi": "ch.suedhang.apps.bdi",
+    "isk": "ch.suedhang.apps.isk",
+    "sci": "ch.suedhang.apps.sci",
+    "whoqol": "com.optinomic.apps.whoqol"
+};
