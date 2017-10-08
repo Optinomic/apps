8fc390d93dbd3589cfa1c9acb950ddcc66d84f09
Beat Ottiger <ottigerb@gmail.com>
2017-10-08 17:45:57 +0200

    fix

--- ch.suedhang.apps.tmt_V3/main.js
+++ ch.suedhang.apps.tmt_V3/main.js
@@ -127,7 +127,7 @@ app.controller('AppCtrl', function($scope, $filter, dataService, scopeDService) 
         var statistics = null;
         if (data_dive !== null) {
             statistics = data_dive.statistics;
-            current_location_n = data_dive.patients.length;
+            current_location_n = statistics.TMTAError.n;
             current_location_n_text = 'N=' + current_location_n;
             current_location_full = current_location_text + ' (' + current_location_n_text + ')';
         } else {
