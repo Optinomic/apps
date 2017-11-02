a6ace9d685d0bba7aec597a43ef0cb20fa652a10
Beat Ottiger <ottigerb@gmail.com>
2017-11-02 23:50:01 +0100

    more examples

--- org.optinomic.export.toolbox/definitions/_default_fields.js
+++ org.optinomic.export.toolbox/definitions/_default_fields.js
@@ -2,12 +2,13 @@
 d.default_fields = [
     { "name": "optinomic_patient_id", "path": "patient_id" },
     { "name": "optinomic_stay_id", "path": "stay_id" },
+    { "name": "optinomic_cis_fid", "path": "stay.cis_fid" },
     { "name": "optinomic_event_id", "path": "event_id" },
     { "name": "optinomic_pum_id", "path": "pum_id" },
     { "name": "optinomic_survey_response_id", "path": "survey_response_id" },
     { "name": "optinomic_patient_gender", "path": "patient.gender" },
     { "name": "optinomic_patient_age_when_filled", "path": "patient.extras.age_when_filled" },
-    { "name": "optinomic_cis_fid", "path": "stay.cis_fid" },
+    { "name": "optinomic_filled", "path": "survey_response.filled" },
     { "name": "optinomic_start", "path": "stay.start" },
     { "name": "optinomic_stop", "path": "stay.stop" }
 ];
