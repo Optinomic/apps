85074e050394176a61d7ee6c4cea94705f19a80e
Beat Ottiger <ottigerb@gmail.com>
2017-11-02 23:17:27 +0100

    ...

--- org.optinomic.export.toolbox/definitions/bscl.js
+++ org.optinomic.export.toolbox/definitions/bscl.js
@@ -1,15 +1,18 @@
 d.bscl = {
     "options": {
         "app_id": "org.optinomic.export.toolbox",
-        "calculation_id": "tmt_full",
-        "name": "bscl_full",
+        "calculation_id": "bscl_full",
+        "name": "BSCL",
         "delimitter": ";"
     },
     "fields": [
         { "name": "optinomic_pid", "path": "patient_id" },
+        { "name": "Patient", "path": "patient.extras.full_name" },
+        { "name": "age_when_filled", "path": "patient.extras.age_when_filled" },
         { "name": "optinomic_fid", "path": "stay_id" },
-        { "name": "offensivitaet_scale_score", "path": "calculation.all_results.offensivit__t_scale_score" },
-        { "name": "offensivitaet_sum_score", "path": "calculation.all_results.offensivit__t_sum_score" },
-        { "name": "offensivitaet_z_score", "path": "calculation.all_results.offensivit__t_z_score" }
+        { "name": "cis_fid", "path": "stay.cis_fid" },
+        { "name": "Eintritt", "path": "stay.extras.beginn" },
+        { "name": "Austritt", "path": "stay.extras.ende" },
+        { "name": "Anzahl_Tage", "path": "stay.extras.duration" }
     ]
 };
--- org.optinomic.export.toolbox/definitions/tmt.js
+++ org.optinomic.export.toolbox/definitions/tmt.js
@@ -7,9 +7,18 @@ d.tmt = {
     },
     "fields": [
         { "name": "optinomic_pid", "path": "patient_id" },
+        { "name": "Patient", "path": "patient.extras.full_name" },
+        { "name": "age_when_filled", "path": "patient.extras.age_when_filled" },
         { "name": "optinomic_fid", "path": "stay_id" },
-        { "name": "offensivitaet_scale_score", "path": "calculation.all_results.offensivit__t_scale_score" },
-        { "name": "offensivitaet_sum_score", "path": "calculation.all_results.offensivit__t_sum_score" },
-        { "name": "offensivitaet_z_score", "path": "calculation.all_results.offensivit__t_z_score" }
+        { "name": "cis_fid", "path": "stay.cis_fid" },
+        { "name": "Eintritt", "path": "stay.extras.beginn" },
+        { "name": "Austritt", "path": "stay.extras.ende" },
+        { "name": "Anzahl_Tage", "path": "stay.extras.duration" },
+        { "name": "TMTA_Time", "path": "calculation.TMTATime" },
+        { "name": "TMTA_Error", "path": "calculation.TMTAError" },
+        { "name": "TMTA_Z", "path": "calculation.percentile.z_scores.tmtA_z_rounded" },
+        { "name": "TMTB_Time", "path": "calculation.TMTBTime" },
+        { "name": "TMTB_Error", "path": "calculation.TMTBError" },
+        { "name": "TMTB_Z", "path": "calculation.percentile.z_scores.tmtB_z_rounded" }
     ]
 };
