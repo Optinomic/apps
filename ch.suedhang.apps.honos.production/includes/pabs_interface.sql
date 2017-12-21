80c89a26738cbaa8d95ebf7fb7505cfcb07e51f7
Beat Ottiger <ottigerb@gmail.com>
2017-12-21 09:47:55 +0100

    HONOS :: PABS Export :: no empty cis_fid

--- ch.suedhang.apps.honos.production/includes/pabs_interface.sql
+++ ch.suedhang.apps.honos.production/includes/pabs_interface.sql
@@ -9,7 +9,7 @@ SELECT
   survey_response_view.filled as optinomic_survey_filled,
   ((cast(response AS json))->>'id') as optinomic_limesurvey_id,
   -- END:  Optinoimc Default |  Needed for Export-Toolbox
-  
+
   patient.cis_pid as cis_pid,
   CONCAT(patient.cis_pid, '00', RIGHT((stay.cis_fid/100)::text,2)) as medstatfid,
   stay.cis_fid/100 as fid,
@@ -22,11 +22,11 @@ SELECT
   CASE WHEN ((cast(response AS json))->>'q401V04')~E'^\\d+$' THEN ((cast(response AS json))->>'q401V04')::integer ELSE 3 END as zeitpunkt_honos,
   ((cast(response AS json))->>'q401V05') as dropoutcode_honos,
   ((cast(response AS json))->>'q401V06') as spezifikation_dropout_honos_andere,
-  
-  CASE (CASE WHEN ((cast(response AS json))->>'q401V04')~E'^\\d+$' THEN ((cast(response AS json))->>'q401V04')::integer ELSE 3 END) 
-  WHEN 1 THEN to_char(stay.start, 'YYYYMMDD') 
-  WHEN 2 THEN to_char(stay.stop, 'YYYYMMDD')  
-  ELSE TO_CHAR(TO_DATE(((cast(response AS json))->>'q402V00'), 'YYYY-MM-DD'), 'YYYYMMDD') 
+
+  CASE (CASE WHEN ((cast(response AS json))->>'q401V04')~E'^\\d+$' THEN ((cast(response AS json))->>'q401V04')::integer ELSE 3 END)
+  WHEN 1 THEN to_char(stay.start, 'YYYYMMDD')
+  WHEN 2 THEN to_char(stay.stop, 'YYYYMMDD')
+  ELSE TO_CHAR(TO_DATE(((cast(response AS json))->>'q402V00'), 'YYYY-MM-DD'), 'YYYYMMDD')
   END as datum_erhebung_honos,
 
   ((cast(response AS json))->>'H1[402V01]') as honos_h1,
@@ -38,17 +38,18 @@ SELECT
   ((cast(response AS json))->>'H1[402V07]') as honos_h7,
   ((cast(response AS json))->>'H1[402V08]') as honos_h8,
   ((cast(response AS json))->>'q402V09') as honos_h8a,
-  ((cast(response AS json))->>'q402V10') as honos_h8b,  
+  ((cast(response AS json))->>'q402V10') as honos_h8b,
   ((cast(response AS json))->>'H2[402V11]') as honos_h9,
   ((cast(response AS json))->>'H2[402V12]') as honos_h10,
   ((cast(response AS json))->>'H2[402V13]') as honos_h11,
   ((cast(response AS json))->>'H2[402V14]') as honos_h12
-FROM "survey_response_view" 
-LEFT JOIN patient ON(survey_response_view.patient_id = patient.id) 
+FROM "survey_response_view"
+LEFT JOIN patient ON(survey_response_view.patient_id = patient.id)
 LEFT JOIN stay ON(survey_response_view.stay_id = stay.id)
 WHERE module = 'ch.suedhang.apps.honos.production'
 AND date_trunc('day', (((cast(response AS json))->>'q402V00')::timestamp) = date_trunc('day', now())
 AND ((cast(response AS json))->>'q401V04') != ''
+AND stay.cis_fid is not null
 AND survey_response_view.patient_id != '1169'
 AND survey_response_view.patient_id != '387'
 AND survey_response_view.patient_id != '1'
